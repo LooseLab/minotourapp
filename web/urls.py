a6317162ac9d543d673212ac22cb7d5cd675f19c
Matt Loose <matt.loose@nottingham.ac.uk>
2017-08-08 10:35:54 +0100

    added a profile page to display user options

--- web/urls.py
+++ web/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     url(r'^private/index/$', views.private_index, name='private-index'),
     url(r'^private/external-links/$', views.external_links, name='external-links'),
     url(r'^private/minup/$', views.minup, name='minup'),
+    url(r'^private/profile/$', views.profile, name='profile'),
     url(r'^private/tutorial/$', views.tutorial, name='tutorial'),
     url(r'^private/previous-runs/$', views.previous_run, name='previous-runs'),
     url(r'^private/current-runs/$', views.current_run, name='current-runs'),
