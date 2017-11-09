9f4dd1b312bf84b86fe7f83900caafac6fe583fc
Matt Loose <matt.loose@nottingham.ac.uk>
2017-11-09 23:12:51 +0000

    fixing tabs - some kind of problem

--- tabs/urls.py
+++ tabs/urls.py
@@ -3,13 +3,15 @@ from django.conf.urls import url
 from tabs import views
 
 urlpatterns = [
-    url(
-        r'^tabs/$',
-        views.tabs,
-        name="tabs"),
-    url(
-        r'^tabs/(?P<pk>[0-9]+)/$',
-        views.tabs_details,
-        name="tabs-details"
-    ),
+
+    #url(
+    #    r'^tabs/$',
+    #    views.tabs,
+    #    name="tabs"),
+    #url(
+    #    r'^tabs/(?P<pk>[0-9]+)/$',
+    #    views.tabs_details,
+    #    name="tabs-details"
+    #),
+
 ]
