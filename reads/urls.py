fe87667d0a41928c495fe6bcd3b1036418efbbae
Alex Payne <alexander.payne@nottingham.ac.uk>
2017-11-16 13:08:56 +0000

    Tabs

--- reads/urls.py
+++ reads/urls.py
@@ -201,6 +201,7 @@ urlpatterns = [
         views.flowcell_channel_summary,
         name="flowcellchannelsummary-list"
     ),
+    url(
         r'^api/v1/flowcells/(?P<pk>[0-9]+)/rundetails/$',
         views.flowcell_run_status_list,
         name="flowcellrunstatus_list"
