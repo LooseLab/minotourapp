88a653e3232dcff9cd96a5ff449a2052322d0aef
Roberto Santos <robertosantos@pobox.com>
2018-01-31 16:37:47 +0000

    Add reset password.

--- minotourapp/urls.py
+++ minotourapp/urls.py
@@ -15,11 +15,13 @@ urlpatterns = [
 
     url(r'^change-password-done/$', auth_views.PasswordChangeDoneView.as_view(template_name="registration/password_change_done.html"), name="password_change_done"),
 
-    url(r'^reset-password/$', auth_views.PasswordResetView.as_view(template_name="registration/password_reset.html"), name="password_reset"),
+    url(r'^password-reset/$', auth_views.password_reset, name="password_reset"),
 
-    url(r'^reset-password-done/$', auth_views.PasswordResetDoneView.as_view(template_name="registration/password_reset_done.html"), name="password_reset_done"),
+    url(r'^password-reset-done/$', auth_views.password_reset_done, name="password_reset_done"),
 
-    url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', auth_views.PasswordResetConfirmView.as_view(template_name="registration/password_reset_confirm.html"), name='password_reset_confirm'),
+    url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', auth_views.password_reset_confirm, name='password_reset_confirm'),
+
+    url(r'^reset/done/$', auth_views.password_reset_complete, name='password_reset_complete'),
 
     url(r'^admin/', admin.site.urls),
 
