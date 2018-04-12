4d5578b507fd5aa106cfed423835a7498a265a8b
Roberto Santos <robertosantos@pobox.com>
2018-04-12 15:15:14 +0100

    Remove non used code.

--- reads/services.py
+++ reads/services.py
@@ -1,26 +1,4 @@
-from devices.models import Flowcell
 from reads.models import RunSummaryBarcode, RunStatisticBarcode, HistogramSummary, ChannelSummary
-
-
-def update_flowcelldetails(flowcell_id):
-
-    flowcell = Flowcell.objects.get(pk=flowcell_id)
-
-    flowcell_details = flowcell.flowcell_details
-
-    # check if flowcell has barcoded run
-
-    flowcellruns = flowcell.flowcelldetails
-
-    flowcell_details.has_barcode = False
-
-    for flowcellrun in flowcellruns.all():
-        run = flowcellrun.run
-        if run.is_barcoded:
-            flowcell_details.has_barcode = True
-            break
-
-    flowcell_details.save()
 
 
 def save_runsummarybarcode(run_id, row):
@@ -34,18 +12,6 @@ def save_runsummarybarcode(run_id, row):
     quality_average_sum = row['quality_average']['sum']
     read_count = row['sequence_length']['count']
     channels = row['channel']['unique']
-
-    """print("barcode_id: {}".format(barcode_id))
-    print("type_id: {}".format(type_id))
-    print("is_pass: {}".format(is_pass))
-    print("sequence_length min: {}".format(row['sequence_length']['min']))
-    print("sequence_length max: {}".format(row['sequence_length']['max']))
-    print("sequence_length sum: {}".format(row['sequence_length']['sum']))
-    print("count: {}".format(row['sequence_length']['count']))
-    print("quality average sum: {}".format(row['quality_average']['sum']))
-    print("channels: ")
-    print(channels)
-    print("channel list: {}".format(''.join(channel_list)))"""
 
     runSummaryBarcode, created = RunSummaryBarcode.objects.get_or_create(
         run_id_id = run_id,
