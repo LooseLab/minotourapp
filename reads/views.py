eb90ccb154b771d619ee0f86c96a014032eec819
Matt Loose <mattloose@DestroyerOfWorlds.local>
2018-05-17 14:47:01 +0100

    added comments to code modificaitons

--- reads/views.py
+++ reads/views.py
@@ -1497,11 +1497,13 @@ def flowcell_histogram_summary(request, pk):
     df = pd.DataFrame.from_records(qs.values('barcode__name', 'read_type__name', 'is_pass', 'read_count', 'read_length', 'bin_index'))
 
     gb = df.groupby(['barcode__name', 'read_type__name', 'is_pass', 'bin_index']).agg({'read_count': ['sum'], 'read_length': ['sum']})
+    ## new_index should get the maximum value in the data to rebuild the histogram dataset
     new_index = list(range(0, gb.index.values.max()[3] + 1))
-    gb2 = gb.unstack(['barcode__name'])
-    gb3 = gb2.unstack(['read_type__name'])
-    gb4 = gb3.unstack(['is_pass'])
-    gb = gb4.reindex(new_index, fill_value=0).stack(['is_pass', 'read_type__name', 'barcode__name']).reorder_levels(
+    ## Here we unstack the multiindex datafram so we can reindex the bin_index.
+    ## We do this using the new_index and we fill in the blanks with 0 values.
+    ## We then restack the data to rebuild the multindex.
+    ## Finally we reorder the indexes to return the order to the original
+    gb = gb.unstack(['barcode__name', 'read_type__name', 'is_pass']).reindex(new_index, fill_value=0).stack(['is_pass', 'read_type__name', 'barcode__name']).reorder_levels(
         ['barcode__name', 'read_type__name', 'is_pass', 'bin_index']).sortlevel(level=0)
 
     payload = gb.reset_index().apply(lambda row: (row['barcode__name'][0], '{} {} {}'.format(row['barcode__name'][0], row['read_type__name'][0], 'Pass' if row['is_pass'][0] == True else 'Fail'), row['bin_index'][0] * 900 + 900, row['read_count']['sum'], row['read_length']['sum']), axis=1)
