6958855525ce8ada76fd489f84e26c7838b3367c
Roberto Santos <robertosantos@pobox.com>
2018-08-02 16:17:53 +0100

    Remove minikraken app.

--- minikraken/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from minikraken.models import MiniKraken, ParsedKraken
-
-#admin.site.register(MiniKraken)
-#admin.site.register(TestKraken)
