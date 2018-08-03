9dd02dec82bcd1022c1026d3d6cf0e5c4d804fc4
Roberto Santos <robertosantos@pobox.com>
2018-08-03 09:39:59 +0100

    Comment some centrifuge urls that are in conflict.

--- centrifuge/urls.py
+++ centrifuge/urls.py
@@ -4,18 +4,18 @@ from centrifuge.views import CentViewSet, cent_donut, start_centrifuge_view, Def
     get_or_set_cartmap, RefViewSet, cent_sankey, vis_table_data, metaview
 
 # django rest framework router
-router = DefaultRouter()
-router.register(prefix="cent", viewset=CentViewSet, base_name="cent")
-router.register(prefix="defaulttargets", viewset=DefaultViewSet, base_name="default")
-router.register(prefix="ref", viewset=RefViewSet, base_name="references")
+# router = DefaultRouter()
+# router.register(prefix="cent", viewset=CentViewSet, base_name="cent")
+# router.register(prefix="defaulttargets", viewset=DefaultViewSet, base_name="default")
+# router.register(prefix="ref", viewset=RefViewSet, base_name="references")
 
 urlpatterns = [
     url(r"^start/$", start_centrifuge_view),
-    url("sankey", cent_sankey),
-    url("cartmaptargets", get_or_set_cartmap),
-    url("donut", cent_donut),
-    url("table", vis_table_data),
-    url("metaview", metaview)
+    url(r"^sankey", cent_sankey),
+    url(r"^cartmaptargets", get_or_set_cartmap),
+    url(r"^donut", cent_donut),
+    url(r"^table", vis_table_data),
+    url(r"^metaview", metaview)
 ]
 
-urlpatterns += router.urls
+# urlpatterns += router.urls
