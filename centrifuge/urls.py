d091d17d1b4dc1bbc1a00d3c51cc4211d8d1b981
Roberto Santos <robertosantos@pobox.com>
2018-09-10 16:32:28 +0100

    Fix url formats.

--- centrifuge/urls.py
+++ centrifuge/urls.py
@@ -7,9 +7,9 @@ from centrifuge.views import CentViewSet, start_centrifuge_view, DefaultViewSet,
 
 urlpatterns = [
     url(r"^start/$", start_centrifuge_view),
-    url("sankey", cent_sankey_two),
-    url("cartmaptargets", get_or_set_cartmap),
-    url("donut", vis_table_data),
-    url("table", vis_table_data),
-    url("metaview", metaview)
+    url(r"^sankey/$", cent_sankey_two),
+    url(r"^cartmaptargets/$", get_or_set_cartmap),
+    url(r"^donut/$", vis_table_data),
+    url(r"^table/$", vis_table_data),
+    url(r"^metaview/$", metaview)
 ]
