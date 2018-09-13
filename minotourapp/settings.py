f1b8ff593892798c47b889c4157bd3ce465f269c
adoni5 <roryjmunro1@gmail.com>
2018-09-13 08:50:59 +0100

    Tried, Tested and Documented, a true story of real life heroism.

--- minotourapp/settings.py
+++ minotourapp/settings.py
@@ -20,7 +20,6 @@ from minotourapp.utils import get_env_variable
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
--- web/static/web/js/vis-d3/AllResultsTable.js
+++ web/static/web/js/vis-d3/AllResultsTable.js
@@ -25,16 +25,16 @@ function getTotalReadsTable(flowCellId) {
             return;
         }
         // If the table already exists, use the DataTable APi to update in place
-        if (table.length !== 1) {
+        console.log(table.length);
+        console.log(table);
+        if ($.fn.DataTable.isDataTable(table)) {
             table.DataTable().clear();
             table.DataTable().rows.add(result);
             table.DataTable().draw();
         } else {
             // else the databale must be initialised
-            table = $(".tableLand").DataTable({
-                    data: result, scrollY: "500px",
-                    paging: false,
-                    select: "single",
+            table.DataTable({
+                    data: result,
                     columns: [
                         {"data": "superkingdom"},
                         {"data": "phylum"},
@@ -49,32 +49,6 @@ function getTotalReadsTable(flowCellId) {
                 }
             );
         }
-        // $('.tableLand tbody').on('click', 'td.details-control', function () {
-        //     var tr = $(this).closest('tr');
-        //     var tdi = tr.find("i.fa");
-        //     var row = table.row(tr);
-        //
-        //     if (row.child.isShown()) {
-        //         // This row is already open - close it
-        //         row.child.hide();
-        //         tr.removeClass('shown');
-        //         tdi.first().removeClass('fa-minus-square');
-        //         tdi.first().addClass('fa-plus-square');
-        //     }
-        //     else {
-        //         // Open this row
-        //         row.child(format(row.data())).show();
-        //         tr.addClass('shown');
-        //         tdi.first().removeClass('fa-plus-square');
-        //         tdi.first().addClass('fa-minus-square');
-        //     }
-        // });
-        //
-        // table.on("user-select", function (e, dt, type, cell, originalEvent) {
-        //     if ($(cell.node()).hasClass("details-control")) {
-        //         e.preventDefault();
-        //     }
-        // });
     });
 
 }
