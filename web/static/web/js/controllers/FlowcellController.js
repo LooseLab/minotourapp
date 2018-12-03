b51dea32df424eaad8f55f25a86b39f23fb0498a
Roberto Santos <robertosantos@pobox.com>
2018-12-03 16:59:08 +0000

    Move coverage chart to controller/model config.

--- web/static/web/js/controllers/FlowcellController.js
+++ web/static/web/js/controllers/FlowcellController.js
@@ -5,10 +5,16 @@ class FlowcellController {
         this._flowcell = new Flowcell(flowcell_id);
 
         this._flowcell_tab_controller = new FlowcellTabController(flowcell_id);
+        this._coverage_chart_controller = new CoverageChartController('coverage_div');
     }
 
     get flowcell_tab_controller() {
 
         return this._flowcell_tab_controller;
+    }
+
+    get coverage_chart_controller() {
+
+        return this._coverage_chart_controller;
     }
 }
