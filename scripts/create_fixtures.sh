7b111d128301a04819c486a444d51c55faa55cdf
mattloose <matt.loose@nottingham.ac.uk>
2019-02-22 10:20:51 +0000

    remove incorrect reference addition

--- scripts/create_fixtures.sh
+++ scripts/create_fixtures.sh
@@ -2,7 +2,4 @@ python ../manage.py dumpdata --indent 2 \
     reads.fastqreadtype \
     jobs.jobtype \
     reads.minioneventtype \
-    reference.referenceinfo \
-    reference.referenceline \
-    centrifuge.mappingtarget \
     > ../fixtures/auxiliary_data.json
