9cd11e3d52d924f1fd971c74f14e93d73f7d3f67
Roberto Santos <roberto.santos@nottingham.ac.uk>
2019-03-01 16:25:39 +0000

    Move dockerfiles to docker folder. Add .bat version.

--- celery.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd /var/lib/minotour/apps/minotourapp;
-
-source ./envs.sh
-
-/var/lib/minotour/envs/minotour/bin/python3 /var/lib/minotour/envs/minotour/bin/celery -A minotourapp worker -l info -B --uid 1016;
-
