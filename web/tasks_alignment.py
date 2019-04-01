c8034dc953fcffe3752b68052c62dea220a8e0a6
Matt Loose <mattloose@DestroyerOfWorlds.local>
2019-04-01 10:39:01 +0100

    added in new example function calculating expected benefit over the genome

--- web/tasks_alignment.py
+++ web/tasks_alignment.py
@@ -13,7 +13,7 @@ from alignment.models import PafRoughCov, PafStore, PafSummaryCov
 from jobs.models import JobMaster
 from reads.models import FastqRead, Flowcell
 from reference.models import ReferenceInfo, ReferenceLine
-from web.utils import parse_md_cg_pafline, parseMDPAF
+from web.utils import parse_md_cg_pafline, parseMDPAF, parseMDPAF_alex, getbenefit, rolling_sum, check_mask
 
 logger = get_task_logger(__name__)
 
@@ -339,6 +339,271 @@ def align_reads(fastqs, job_master_id):
         paf_store_gb.reset_index().apply(lambda row: save_paf_store_summary(job_master.id, row), axis=1)
 
     return last_read
+
+
+def calculate_exepected_benefit_2dot0(flowcell_id,job_master_id):
+    REFERENCE_LOCATION = getattr(settings, "REFERENCE_LOCATION", None)
+    MINIMAP2 = getattr(settings, "MINIMAP2", None)
+    CHUNK_SIZE = 2000
+
+    job_master = JobMaster.objects.get(pk=job_master_id)
+
+    last_read = job_master.last_read
+    reference_info_id = job_master.reference.id
+    flowcell = job_master.flowcell
+
+    fastqs = FastqRead.objects.filter(flowcell_id=flowcell_id, id__gt=int(last_read))[:CHUNK_SIZE]
+
+    if not MINIMAP2:
+        logger.error('Can not find minimap2 executable - stopping task.')
+        return
+
+    if last_read is None:
+        last_read = 0
+
+    reference_info = ReferenceInfo.objects.get(pk=reference_info_id)
+
+    chromdict = dict()
+
+    chromosomes = reference_info.referencelines.all()
+
+    for chromosome in chromosomes:
+        chromdict[chromosome.line_name] = chromosome
+
+    read = ''
+    fastq_dict = dict()
+
+    for fastq in fastqs:
+        read = read + '>{} \r\n{}\r\n'.format(fastq.read_id, fastq.sequence)
+
+        fastq_dict[fastq.read_id] = fastq
+
+        last_read = fastq.id
+
+    cmd = '{} -x map-ont -t 1 --secondary=no -c --MD {} -'.format(
+        MINIMAP2,
+        os.path.join(REFERENCE_LOCATION, reference_info.filename)
+    )
+
+    logger.info('Flowcell id: {} - Calling minimap - {}'.format(flowcell.id, cmd))
+
+    proc = subprocess.Popen(cmd, stdout=subprocess.PIPE,
+                            stderr=subprocess.PIPE,
+                            stdin=subprocess.PIPE, shell=True)
+
+    (out, err) = proc.communicate(input=read.encode("utf-8"))
+
+    status = proc.wait()
+
+    logger.info('Flowcell id: {} - Finished minimap - {}'.format(flowcell.id, cmd))
+
+    paf = out.decode("utf-8")
+
+    pafdata = paf.splitlines()
+
+    logger.info('Flowcell id: {} - Found {} paf records'.format(flowcell.id, len(pafdata)))
+
+    if len(pafdata) > 0:
+
+        paf_summary_cov_dict = dict()
+
+        bulk_paf = []
+
+        bulk_paf_rough = []
+
+        for line in pafdata:
+
+            line = line.strip('\n')
+
+            record = line.split('\t')
+
+            fastq_read = fastq_dict[record[0]]
+
+            typeid = fastq_read.type.id
+
+            is_pass = fastq_read.is_pass
+
+            barcode_name = fastq_read.barcode_name
+
+            newpaf = PafStore(
+                job_master=job_master,
+                read=fastq_read,
+            )
+
+            newpafstart = PafRoughCov(
+                job_master=job_master,
+                flowcell=flowcell,
+                read_type=fastq_read.type,
+                barcode_name=fastq_read.barcode_name,
+                is_pass=fastq_read.is_pass
+            )
+
+            newpafend = PafRoughCov(
+                job_master=job_master,
+                flowcell=flowcell,
+                read_type=fastq_read.type,
+                barcode_name=fastq_read.barcode_name,
+                is_pass=fastq_read.is_pass
+            )
+
+            newpaf.reference = reference_info
+
+            newpaf.qsn = fastq_read.read_id  # models.CharField(max_length=256)#1	string	Query sequence name
+            newpaf.qsl = int(record[1])  # models.IntegerField()#2	int	Query sequence length
+            newpaf.qs = int(record[2])  # models.IntegerField()#3	int	Query start (0-based)
+            newpaf.qe = int(record[3])  # models.IntegerField()#4	int	Query end (0-based)
+            newpaf.rs = record[4]  # models.CharField(max_length=1)#5	char	Relative strand: "+" or "-"
+            newpaf.tsn = chromdict[record[5]]  # models.CharField(max_length=256)#6	string	Target sequence name
+            newpaf.tsl = int(record[6])  # models.IntegerField()#7	int	Target sequence length
+            newpaf.ts = int(record[7])  # models.IntegerField()#8	int	Target start on original strand (0-based)
+            newpaf.te = int(record[8])  # models.IntegerField()#9	int	Target end on original strand (0-based)
+            newpaf.nrm = int(record[9])  # models.IntegerField()#10	int	Number of residue matches
+            newpaf.abl = int(record[10])  # models.IntegerField()#11	int	Alignment block length
+            newpaf.mq = int(record[11])  # models.IntegerField()#12	int	Mapping quality (0-255; 255 for missing)
+
+            newpafstart.reference = reference_info
+            newpafstart.chromosome = chromdict[record[5]]
+            newpafstart.p = int(record[7])
+            newpafstart.i = 1
+
+            newpafend.reference = reference_info
+            newpafend.chromosome = chromdict[record[5]]
+            newpafend.p = int(record[8])
+            newpafend.i = -1
+
+            bulk_paf_rough.append(newpafstart)
+            bulk_paf_rough.append(newpafend)
+
+            bulk_paf.append(newpaf)
+
+            if reference_info not in paf_summary_cov_dict:
+                paf_summary_cov_dict[reference_info] = dict()
+
+            if chromdict[record[5]] not in paf_summary_cov_dict[reference_info]:
+                paf_summary_cov_dict[reference_info][chromdict[record[5]]] = dict()
+
+            if barcode_name not in paf_summary_cov_dict[reference_info][chromdict[record[5]]]:
+                paf_summary_cov_dict[reference_info][chromdict[record[5]]][barcode_name] = dict()
+
+            if typeid not in paf_summary_cov_dict[reference_info][chromdict[record[5]]][barcode_name]:
+                paf_summary_cov_dict[reference_info][chromdict[record[5]]][barcode_name][typeid] = dict()
+
+            if 'read' not in paf_summary_cov_dict[reference_info][chromdict[record[5]]][barcode_name][typeid]:
+                paf_summary_cov_dict[reference_info][chromdict[record[5]]][barcode_name][typeid]['read'] = set()
+                paf_summary_cov_dict[reference_info][chromdict[record[5]]][barcode_name][typeid]['length'] = 0
+
+            paf_summary_cov_dict[reference_info][chromdict[record[5]]][barcode_name][typeid]['read'].add(record[0])
+            paf_summary_cov_dict[reference_info][chromdict[record[5]]][barcode_name][typeid]['length'] += int(
+                record[3]) - int(
+                record[2]) + 1
+
+        PafStore.objects.bulk_create(bulk_paf, batch_size=1000)
+        PafRoughCov.objects.bulk_create(bulk_paf_rough, batch_size=1000)
+
+        paf_store_list = PafStore.objects.filter(
+            job_master=job_master
+        ).values(
+            'job_master__id',
+            'read__barcode_name',
+            'tsn__line_name',
+            'qsn',
+            'qs',
+            'qe'
+        )
+
+        paf_store_df = pd.DataFrame.from_records(paf_store_list)
+
+        paf_store_df['length'] = paf_store_df['qe'] - paf_store_df['qs']
+
+        paf_store_gb = paf_store_df.groupby(
+            ['job_master__id', 'read__barcode_name', 'tsn__line_name']).agg(
+            {'qsn': ['unique'], 'length': ['sum']})
+
+        paf_store_gb.reset_index().apply(lambda row: save_paf_store_summary(job_master.id, row), axis=1)
+
+    ### Now reprocess the pafdata and read the cigar for expected benefit.
+
+    # Create dictionaries if not passed in.
+    # These dictionaries somehow need to persist from one iteration to the next.
+
+    # Dictionary to hold matches, mismatches and coverage per base. Structure referencedict[chromosome][match/mismatch/coverage][position]=[int]
+    referencedict = dict()
+    # Dictionary to hold benefit. Structure benefitdict[chromosome][position]=[float]
+    benefitdict = dict()
+    # Dictionary to hold rolling benefit, mean benefit and mask.
+    # This is badly structured! The Masks are stored as Bools with position. The rollingbenefit is stored as a float with position. The Means are floats but a single value per chromosome.
+    # Structure rollingdict[chromosome][Forward/Reverse/ForMean/RevMean/ForMask/RevMask][optional position]=float,float,bool
+    rollingdict = dict()
+
+    if referencedict is None:
+        referencedict = dict()
+    if benefitdict is None:
+        benefitdict = dict()
+    if rollingdict is None:
+        rollingdict = dict()
+    readcounter = 0
+    mean = 0
+    readlen = 0
+
+
+    # These are parameters but might be tuned in future.
+    error = 0.1
+    prior_diff = 0.0001
+
+    if len(pafdata) > 0:
+
+        for line in pafdata:
+            # Get the specific arrays of matches, mismatches, start mapping position, end mapping position, mapping orientation, reference(chromosome), reference lenght, readlength
+            # These values are for a single read.
+            # readlength is the total length of the read, not the aligned length.
+            mismatcharray, matcharray, mapstart, mapend, maporientation, reference, referencelength, readlength = parseMDPAF_alex(
+                line)
+            # Calculate the length of the aligned portion we are looking at.
+            readlen += len(mismatcharray)
+            # Increment counter for reads processed on this loop.
+            readcounter += 1
+            # Calculate the mean of reads seen so far.
+            mean = readlen / readcounter
+            # If the chromosome we have seen is not in the reference dictionary, we need to build it and also calculate the initial mask and rolling dictionary.
+            if reference not in referencedict:
+                referencedict, rollingdict = add_chromosome(referencedict, rollingdict, benefitdict, reference,
+                                                            referencelength, mean)
+            # Here we add the mismatch and match scores to the existing scores in the larger reference dictionary. We only update those positions which have changed for this read.
+            referencedict[reference]["mismatch"][mapstart:mapstart+len(mismatcharray)] += mismatcharray
+            referencedict[reference]["match"][mapstart:mapstart+len(matcharray)] += matcharray
+
+
+    ### At this point we should have a dictionary for the reference with the count of matches and mismatches
+    ### Now we need to add this to any previously seen reads.
+    ### Suggest something like:
+    ### for reference in referencedict:
+    ###     fetch old coverage data
+    ###     add new coverage data to old coverage data
+    ###     now calculate getbenefit over the entire reference:
+            # Here we calculate the new benefit for this reference.
+            ### Somewhere we need to specify error and prior_diff - these are variables we might want to change.
+            benefitdict[reference] = getbenefit(
+                referencedict[reference]["match"],
+                referencedict[reference]["mismatch"],
+                error=error, prior_diff=prior_diff)
+            A = benefitdict[reference]
+            #mean is the mean read length at this point.
+            rollingdict[reference]["Forward"] = rolling_sum([A], n=mean, pad=True)[0]
+            rollingdict[reference]["Reverse"] = rolling_sum([A[::-1]], n=mean, pad=True)[0][::-1]
+            # Recalculate the mean values for the reference we are looking at.
+            rollingdict[reference]["ForMean"] = rollingdict[reference]["Forward"].mean()
+            rollingdict[reference]["RevMean"] = rollingdict[reference]["Reverse"].mean()
+            # Update the coverage.
+            referencedict[reference]["coverage"] = np.sum(
+                [referencedict[reference]["match"], referencedict[reference]["mismatch"]], axis=0)
+            # Update the mask for sequencing
+            rollingdict[reference]["ForMask"] = check_mask(rollingdict[reference]["Forward"],
+                                                           rollingdict[reference]["ForMean"])
+            rollingdict[reference]["RevMask"] = check_mask(rollingdict[reference]["Reverse"],
+                                                           rollingdict[reference]["RevMean"])
+
+
+
 
 
 def calculate_expected_benefit(flowcell_id, job_master_id):
