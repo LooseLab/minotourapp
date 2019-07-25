8a46a043bd6bc5b3f8e95e50dcfdfcefdecf4c1c
adoni5 <roryjmunro1@gmail.com>
2019-07-25 11:03:55 +0100

    added rejected read urls

--- readuntil/urls.py
+++ readuntil/urls.py
@@ -17,5 +17,15 @@ urlpatterns = [
         r'^api/v1/readuntil/benefitdata/chromosomes/$',
         views.get_chromosomes,
         name="benefit-data-chromosomes"
+    ),
+    url(
+        r'^api/v1/readuntil/rejected_ids/$',
+        views.get_or_create_rejected_reads,
+        name="rejected_ids"
+    ),
+    url(
+        r'api/v1/reduntil/accepted_ids/$',
+        views.get_or_create_accepted_reads,
+        name="accepted_ids"
     )
 ]
