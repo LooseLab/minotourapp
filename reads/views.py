9e217e62ee85e95ae0f64214eef9d23dc2622fe5
Matt Loose <mattloose@DestroyerOfWorlds.local>
2019-07-26 11:46:57 +0100

    fix quality scores to 1 decimal place. Correct yield over metric.

--- reads/views.py
+++ reads/views.py
@@ -1115,7 +1115,7 @@ def flowcell_statistics(request, pk):
     df3['cumulative_read_count'] = df3.groupby(['barcode_name','read_type_name','read_type'])['read_count'].apply(lambda x: x.cumsum())
     df3['cumulative_bases'] = df3.groupby(['barcode_name','read_type_name', 'read_type'])['total_length'].apply(lambda x: x.cumsum())
     df3['key']=df3['barcode_name'].astype('str') + " - " + df3['read_type_name'].astype('str') + " - " + df3['read_type'].astype('str')
-    df3['average_quality'] = df3['quality_sum'].div(df3['read_count']).astype('float').round(decimals=2)
+    df3['average_quality'] = df3['quality_sum'].div(df3['read_count']).astype('float').round(decimals=1)
     df3['average_quality'] = df3['average_quality'].astype('float')
     df3['average_length'] = df3['total_length'].div(df3['read_count']).round(decimals=0)
     df3['sequence_rate'] = df3['total_length'].div(60).round(decimals=0)
@@ -1207,6 +1207,9 @@ def flowcell_histogram_summary(request, pk):
 
     flowcell = Flowcell.objects.get(pk=pk)
 
+    total_reads_length = flowcell.total_read_length
+    total_reads_count = flowcell.number_reads
+
     max_bin_index = FlowcellHistogramSummary.objects \
         .filter(flowcell=flowcell) \
         .filter(barcode_name=barcode_name) \
@@ -1268,10 +1271,10 @@ def flowcell_histogram_summary(request, pk):
 
             result_collect_read_count_sum = list(
                 (-pd.concat([pd.Series([0]),
-                             pd.Series(result_collect_read_count_sum).replace(to_replace=0, method='ffill')])+pd.Series(result_collect_read_count_sum).replace(to_replace=0, method='ffill').max())/pd.Series(result_collect_read_count_sum).replace(to_replace=0, method='ffill').max()*50)
+                             pd.Series(result_collect_read_count_sum).replace(to_replace=0, method='ffill')])+pd.Series(result_collect_read_count_sum).replace(to_replace=0, method='ffill').max())/total_reads_count*100)
             result_collect_read_length_sum = list(
                 (-pd.concat([pd.Series([0]),
-                             pd.Series(result_collect_read_length_sum).replace(to_replace=0, method='ffill')])+pd.Series(result_collect_read_length_sum).replace(to_replace=0, method='ffill').max())/pd.Series(result_collect_read_length_sum).replace(to_replace=0, method='ffill').max()*50)
+                             pd.Series(result_collect_read_length_sum).replace(to_replace=0, method='ffill')])+pd.Series(result_collect_read_length_sum).replace(to_replace=0, method='ffill').max())/total_reads_length*100)
 
             data_keys[barcode_name][read_type_name][l_is_pass] = {
                 'read_count': {
