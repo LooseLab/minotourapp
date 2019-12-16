27558dd97aa4ab06a5a2c77c05124f5a9dde9667
adoni5 <roryjmunro1@gmail.com>
2019-12-16 10:00:18 +0000

    Fix the creation of notification conditions for reference, chromosome and barcodes

--- communication/views.py
+++ communication/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
-from communication.models import NotificationConditions, ConditionalChromosomes, ConditionalBarcodes
+from communication.models import NotificationConditions
 from communication.models import Message
 from communication.serializers import MessageSerializer
 
@@ -29,6 +29,41 @@ from reads.models import Flowcell, Barcode
 from reference.models import ReferenceInfo, ReferenceLine
 
 
+def create_notification_conditions(flowcell, user, **kwargs):
+    """
+    Create and save a notification conditions
+    Parameters
+    ----------
+    flowcell: reads.models.Flowcell
+        The flowcell these notifications are being created for
+    user:
+        The user that we are creating this flowcell for
+    kwargs: dict
+        Key word arguments to create the flowcell with
+
+    Returns
+    -------
+
+    """
+    print(kwargs)
+    reference = kwargs.get("reference", None)
+    chromosome = kwargs.get("chromosome", None)
+    barcode = kwargs.get("barcode", None)
+    set_repeat = kwargs.get("set_repeat", False)
+    coverage_target = kwargs.get("cov_targ", 0)
+
+    cond = NotificationConditions(notification_type=kwargs["condition"],
+                                  flowcell=flowcell,
+                                  creating_user=user,
+                                  repeat=set_repeat,
+                                  coverage_target=coverage_target,
+                                  conditional_reference=reference,
+                                  chromosome=chromosome,
+                                  barcode=barcode,
+                                  )
+    return cond
+
+
 @api_view(["GET", "POST"])
 def get_or_create_conditions(request):
     """
@@ -47,7 +82,8 @@ def get_or_create_conditions(request):
 
     if request.method == "POST":
         print(request.data)
-        print(type(request))
+        print(type(request.user))
+
 
         flowcell_pk = request.data.get("flowcell", None)
 
@@ -64,36 +100,43 @@ def get_or_create_conditions(request):
         # Create conditions for each contained in the request body
         for condition, value in conditions.items():
 
+            print(f"Condtion is {condition}")
+
             cov_targ = value if condition is "coverage" else None
 
             repeat = True
 
-            reference = None
+            condition_name = condition_lookup[condition]
 
-            if condition is "coverage" and coverage_settings:
+            print(bool(coverage_settings))
+            print(coverage_settings)
+            print(condition)
+
+            if condition == "coverage" and bool(coverage_settings):
+                print("condition is coverage 2")
+
                 repeat = False
-                reference = ReferenceInfo.objects.get(name=coverage_settings["reference"])
 
-            cond = NotificationConditions(notification_type=condition_lookup[condition],
-                                          flowcell=flowcell,
-                                          creating_user=request.user,
-                                          repeat=repeat,
-                                          coverage_target=cov_targ,
-                                          conditional_reference=reference
-                                          )
-            cond.save()
-        if "coverage" in conditions:
-            pass
-            # a = {"barcodes": (ConditionalBarcodes, Barcode, ), "chromosome": (ConditionalChromosomes, ReferenceLine)}
-            # for name, settings in coverage_settings.items():
-            #     if isinstance(settings, list):
-            #         for itemy in settings:
-            #             foreign = a[name][1].objects.get(re)
-            #             a[name][0](
-            #
-            #             )
+                reference = ReferenceInfo.objects.get(pk=coverage_settings["reference"])
+                chromosome = ReferenceLine.objects.get(pk=coverage_settings["chromosome"])
+                kwargs = {"reference": reference, "chromosome": chromosome, "condition": condition_name,
+                          "set_repeat": repeat, "cov_targ": cov_targ}
+                print(f"barcodes are {coverage_settings['barcodes']}")
 
+                if isinstance(coverage_settings["barcodes"], list):
+                    print("making the conditions")
+                    for barcode in coverage_settings["barcodes"]:
+                        kwargs["barcode"] = Barcode.objects.get(pk=barcode)
+                        cond = create_notification_conditions(flowcell, request.user, **kwargs)
+                        print(cond)
+                        cond.save()
 
+            else:
+                print("mux or target")
+                kwargs = {"condition": condition_name, "set_repeat": True}
+                cond = create_notification_conditions(flowcell, request.user, **kwargs)
+                cond.save()
+                print(cond)
 
         return Response("Successfully saved notification conditions.", status=200)
 
@@ -104,7 +147,7 @@ def retrieve_messages(request):
     Retrieve all the messages that have been sent to a user for display.
     Parameters
     ----------
-    request: django.core.handlers.wsgi.WSGIRequest
+    request: rest_framework.request.Request
 
     Returns
     -------
