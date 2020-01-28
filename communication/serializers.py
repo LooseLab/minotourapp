aefe6b059f4a9ee2802599576fdf3d72d3d62e0a
adoni5 <roryjmunro1@gmail.com>
2020-01-28 16:46:37 +0000

    Add Notification serialiser, to retreive the notifications for the table.

--- communication/serializers.py
+++ communication/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from communication.models import Message
+from communication.models import Message, NotificationConditions
 
 
 class MessageSerializer(serializers.ModelSerializer):
@@ -36,3 +36,40 @@ class MessageSerializerComplete(serializers.ModelSerializer):
             'title',
             'content',
         )
+        #
+
+
+class NotificationSerialiser(serializers.ModelSerializer):
+    """
+    Serialiser used to retrieve the Notifications to be displayed in a table.
+    """
+
+    ref_name = serializers.ReadOnlyField(source="conditional_reference.name", default="N/A")
+    chrom_line_name = serializers.ReadOnlyField(source="chromosome.line_name", default="N/A")
+    # Get human readable name of Choice
+    notification_type = serializers.CharField(read_only=True, source='get_notification_type_display')
+
+    class Meta:
+        model = NotificationConditions
+        fields = (
+            "id",
+            "notification_type",
+            "completed",
+            "coverage_target",
+            "upper_limit",
+            "lower_limit",
+            "ref_name",
+            "chrom_line_name"
+
+        )
+
+    def to_representation(self, instance):
+        my_fields = {'upper_limit', 'lower_limit', 'coverage_target'}
+        data = super().to_representation(instance)
+        for field in my_fields:
+            try:
+                if not data[field]:
+                    data[field] = "N/A"
+            except KeyError:
+                pass
+        return data
