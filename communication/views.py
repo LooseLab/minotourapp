17c41f011faf8ac5a37857fb717a4adbf8695d50
adoni5 <roryjmunro1@gmail.com>
2020-01-28 16:48:39 +0000

    Big changes to creating the Notificaion conditions.

--- communication/views.py
+++ communication/views.py
@@ -4,29 +4,54 @@ from rest_framework.response import Response
 
 from communication.models import NotificationConditions
 from communication.models import Message
-from communication.serializers import MessageSerializer
-
-# @api_view(['GET'])
-# def new_messages_list(request):
-#
-#     queryset = Message.objects.filter(recipient=request.user).order_by('-created_date')
-#     serializer = MessageSerializer(queryset, many=True, context={'request': request})
-#     return Response(serializer.data)
-#
-#
-# def message_details(request, pk):
-#
-#     message = Message.objects.get(pk=pk)
-#
-#     return render(
-#         request,
-#         'communication/message.html',
-#         context={
-#            'message': message,
-#         }
-#     )
+from communication.serializers import MessageSerializer, NotificationSerialiser
 from reads.models import Flowcell, Barcode, UserOptions
 from reference.models import ReferenceInfo, ReferenceLine
+
+
+@api_view(['GET'])
+def new_messages_list(request):
+    """
+    Get all the messages that have been sent by the twitter API
+    Parameters
+    ----------
+    request: rest_framework.request.Request
+        Django request framework request object.
+
+    Returns
+    -------
+
+    """
+
+    queryset = Message.objects.filter(recipient=request.user).order_by('-created_date')
+    serializer = MessageSerializer(queryset, many=True, context={'request': request})
+    return Response(serializer.data)
+
+
+
+
+def message_details(request, pk):
+    """
+    Returns the htm
+    Parameters
+    ----------
+    request
+    pk
+
+    Returns
+    -------
+
+    """
+    message = Message.objects.get(pk=pk)
+
+    return render(
+        request,
+        'communication/message.html',
+        context={
+           'message': message,
+        }
+    )
+
 
 
 def create_notification_conditions(flowcell, user, **kwargs):
@@ -43,25 +68,42 @@ def create_notification_conditions(flowcell, user, **kwargs):
 
     Returns
     -------
+    cond: str
+        Any error message (If any.)
 
     """
     print(kwargs)
-    reference = kwargs.get("reference", None)
-    chromosome = kwargs.get("chromosome", None)
-    barcode = kwargs.get("barcode", None)
-    set_repeat = kwargs.get("set_repeat", False)
-    coverage_target = kwargs.get("cov_targ", 0)
+    # reference = kwargs.get("reference", None)
+    # chromosome = kwargs.get("chromosome", None)
+    # barcode = kwargs.get("barcode", None)
+    # set_repeat = kwargs.get("set_repeat", False)
+    # coverage_target = kwargs.get("coverage_target", 0)
 
-    cond = NotificationConditions(notification_type=kwargs["condition"],
-                                  flowcell=flowcell,
-                                  creating_user=user,
-                                  repeat=set_repeat,
-                                  coverage_target=coverage_target,
-                                  conditional_reference=reference,
-                                  chromosome=chromosome,
-                                  barcode=barcode,
-                                  )
-    return cond
+    # Check for my uniqueness
+
+    check = NotificationConditions.objects.filter(flowcell=flowcell,
+                                                  creating_user=user,
+                                                  **kwargs)
+    if check:
+        return f"An identical Condition already exists for the {kwargs.get('notification_type')} condition you are trying to create."
+
+    check_not_excess = 0
+    if not kwargs.get("notification_type") == "cov":
+        check_not_excess = NotificationConditions.objects.filter(flowcell=flowcell,
+                                                                 creating_user=user,
+                                                                 notification_type=kwargs.get("notification_type")).count()
+
+    if check_not_excess > 2:
+        return f"Two many conditions of type {kwargs.get('notification_type')} already exist on this flowcell." \
+               f" Please delete an existing one."
+
+    if not check or not check_not_excess > 2:
+        cond = NotificationConditions(
+            flowcell=flowcell,
+            creating_user=user,
+            **kwargs
+        )
+        cond.save()
 
 
 @api_view(["GET", "POST"])
@@ -78,12 +120,18 @@ def get_or_create_conditions(request):
     -------
     rest_framework.response.Response
     """
-    condition_lookup = {"coverage": "cov", "newTarget": "targ", "mux": "mux"}
+    condition_lookup = {
+        "coverage": "cov",
+        "newTarget": "targ",
+        "Mux alerts": "mux",
+        "Voltage": "volt",
+        "Temperature": "temp",
+        "Warnings/Errors": "w/e",
+        "Occupancy": "occu",
+        "MinoTour": "mino",
+    }
 
     if request.method == "POST":
-        print(request.data)
-        print(type(request.user))
-
 
         flowcell_pk = request.data.get("flowcell", None)
 
@@ -93,58 +141,100 @@ def get_or_create_conditions(request):
 
         coverage_settings = request.data.get("coverage_sets", {})
 
+        auto_choices = request.data.get("autoChoices", [])
+
+        auto_values = request.data.get("rangeValues", [])
+
         twitter_settings = UserOptions.objects.get(owner=request.user)
 
         tweet = twitter_settings.tweet
 
         handle = twitter_settings.twitterhandle
 
+        errors_list = []
+
         if flowcell is None:
             return Response("No flowcell submitted", status=400)
         if conditions is None:
             return Response("No conditions submitted", status=400)
         if not tweet and bool(handle):
-            return Response("Please provide Twitter permission in the profile section, and set a handle.", status=400)
+            return Response(
+                "Please provide Twitter permission in the profile section, and set a handle.",
+                status=400,
+            )
         # Create conditions for each contained in the request body
         for condition, value in conditions.items():
 
-            print(f"Condtion is {condition}")
+            print(f"Condition is {condition}")
+            print(f"Value is {value}")
 
-            cov_targ = value if condition is "coverage" else None
+            cov_targ = value if condition == "coverage" else None
+
 
             condition_name = condition_lookup[condition]
 
-            print(bool(coverage_settings))
-            print(coverage_settings)
-            print(condition)
-
-            if condition == "coverage" and bool(coverage_settings):
-                print("condition is coverage 2")
+            if condition == "coverage" and bool(coverage_settings) and value:
 
                 repeat = False
 
                 reference = ReferenceInfo.objects.get(pk=coverage_settings["reference"])
-                chromosome = ReferenceLine.objects.get(pk=coverage_settings["chromosome"])
-                kwargs = {"reference": reference, "chromosome": chromosome, "condition": condition_name,
-                          "set_repeat": repeat, "cov_targ": cov_targ}
-                print(f"barcodes are {coverage_settings['barcodes']}")
+                chromosome = ReferenceLine.objects.get(
+                    pk=coverage_settings["chromosome"]
+                )
+                kwargs = {
+                    "conditional_reference": reference,
+                    "chromosome": chromosome,
+                    "notification_type": condition_name,
+                    "repeat": repeat,
+                    "coverage_target": cov_targ,
+                }
 
                 if isinstance(coverage_settings["barcodes"], list):
-                    print("making the conditions")
+
                     for barcode in coverage_settings["barcodes"]:
                         kwargs["barcode"] = Barcode.objects.get(pk=barcode)
-                        cond = create_notification_conditions(flowcell, request.user, **kwargs)
-                        print(cond)
-                        cond.save()
+                        cond = create_notification_conditions(
+                            flowcell, request.user, **kwargs
+                        )
+                        errors_list.append(cond)
 
-            else:
-                print("mux or target")
-                kwargs = {"condition": condition_name, "set_repeat": True}
-                cond = create_notification_conditions(flowcell, request.user, **kwargs)
-                cond.save()
-                print(cond)
+            # Mux or new target
+        for condition in auto_choices:
+            upper_limit = None
+            lower_limit = None
+            defaults = {"Occupancy": [40, 100], "Voltage": [-180, -160]}
+            if condition in ["Occupancy", "Voltage"]:
+                upper_limit = auto_values.get(condition, {}).get("upper", defaults[condition][1])
+                lower_limit = auto_values.get(condition, {}).get("lower", defaults[condition][0])
+            condition_name = condition_lookup[condition]
+            kwargs = {
+                "notification_type": condition_name,
+                "repeat": True,
+                "upper_limit": upper_limit,
+                "lower_limit": lower_limit,
+            }
+            cond = create_notification_conditions(flowcell, request.user, **kwargs)
+            errors_list.append(cond)
+        return_list = ["Successfully created notification conditions.", "Issues:"]
+        return_list.extend(errors_list)
+        return_list = list(filter(None, return_list))
+        return_string = "<br/><br/>".join(return_list)
+        print(return_string)
+        return Response(return_string, status=200)
 
-        return Response("Successfully saved notification conditions.", status=200)
+    elif request.method == "GET":
+
+        flowcell_id = request.GET.get("flowcellId", -1)
+
+        if flowcell_id == -1:
+            return Response("Flowcell ID not provided.", status=400)
+
+        queryset = NotificationConditions.objects.filter(creating_user=request.user, flowcell_id=flowcell_id)
+
+        notifications_serialiser = NotificationSerialiser(queryset, many=True)
+        print(notifications_serialiser.data)
+
+        return Response({"data": notifications_serialiser.data}, status=200)
 
 
 @api_view(["GET"])
