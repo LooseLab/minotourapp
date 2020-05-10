50eada49cad9a1d8b9b42a9f3f1b760a09828ba8
Matt <matt.loose@nottingham.ac.uk>
2020-05-10 20:56:42 +0100

    functionalise

--- web/tasks_move_reads_to_flowcell.py
+++ web/tasks_move_reads_to_flowcell.py
@@ -71,28 +71,32 @@ def move_reads_to_flowcell(run_id, flowcell_id, fromflowcell_id):
 
     ##Update tasks on old flowcell
 
-    #We need to delete the old objects for this flowcell
 
-    FlowcellSummaryBarcode.objects.filter(flowcell=flowcellold).delete()
-    FlowcellStatisticBarcode.objects.filter(flowcell=flowcellold).delete()
-    FlowcellHistogramSummary.objects.filter(flowcell=flowcellold).delete()
-    FlowcellChannelSummary.objects.filter(flowcell=flowcellold).delete()
+    reset_flowcell_info(flowcellold)
 
-    flowcellold.start_time = datetime.datetime.now()
-    flowcellold.max_channel = 0
-    flowcellold.number_reads = 0
-    flowcellold.number_runs = 0
-    flowcellold.number_barcodes = 0
-    flowcellold.average_length = 0
-    flowcellold.total_read_length = 0
-    flowcellold.has_fastq = False
-    flowcellold.save()
+
+def reset_flowcell_info(flowcell):
+    # We need to delete the old objects for this flowcell
+    FlowcellSummaryBarcode.objects.filter(flowcell=flowcell).delete()
+    FlowcellStatisticBarcode.objects.filter(flowcell=flowcell).delete()
+    FlowcellHistogramSummary.objects.filter(flowcell=flowcell).delete()
+    FlowcellChannelSummary.objects.filter(flowcell=flowcell).delete()
+
+    flowcell.start_time = datetime.datetime.now()
+    flowcell.max_channel = 0
+    flowcell.number_reads = 0
+    flowcell.number_runs = 0
+    flowcell.number_barcodes = 0
+    flowcell.average_length = 0
+    flowcell.total_read_length = 0
+    flowcell.has_fastq = False
+    flowcell.save()
 
     # recalculate old flowcell
 
     chunksize = 100000
 
-    totalreads = FastqRead.objects.filter(flowcell=flowcellold).count()
+    totalreads = FastqRead.objects.filter(flowcell=flowcell).count()
 
     steps = int((totalreads - totalreads % chunksize) / chunksize + 1)
 
@@ -102,7 +106,7 @@ def move_reads_to_flowcell(run_id, flowcell_id, fromflowcell_id):
 
         modend = (i + 1) * chunksize
 
-        reads = FastqRead.objects.filter(flowcell=flowcellold)[modstart:modend]
+        reads = FastqRead.objects.filter(flowcell=flowcell)[modstart:modend]
 
         reads_len = reads.count()
 
@@ -119,10 +123,7 @@ def move_reads_to_flowcell(run_id, flowcell_id, fromflowcell_id):
         flowcell.name
     ))
 
-    print('Finished reads from run {} to flowcell {}'.format(
-        run.runid,
-        flowcell.name
-    ))
+
 
     #chancalcjob = flowcellold.flowcelljobs.get(job_type__name="ChanCalc")
     #chancalcjob.last_read=0
