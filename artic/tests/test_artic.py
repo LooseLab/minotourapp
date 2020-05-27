ecd3a2da34db5c5cb02c82e8b57f2787514440c2
adoni5 <roryjmunro1@gmail.com>
2020-05-27 17:45:27 +0100

    Toying with testing for github actions. To be continued on another branch

---
+++ artic/tests/test_artic.py
@@ -0,0 +1,25 @@
+from django.test import TestCase
+
+# Create your tests here.
+
+class YourTestClass(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        print("setUpTestData: Run once to set up non-modified data for all class methods.")
+        pass
+
+    def setUp(self):
+        print("setUp: Run once for every test method to setup clean data.")
+        pass
+
+    def test_false_is_false(self):
+        print("Method: test_false_is_false.")
+        self.assertFalse(False)
+
+    def test_false_is_true(self):
+        print("Method: test_false_is_true.")
+        self.assertTrue(False)
+
+    def test_one_plus_one_equals_two(self):
+        print("Method: test_one_plus_one_equals_two.")
+        self.assertEqual(1 + 1, 2)
