99b33611c1ebf3043db7f0a88d70d434ab2140b5
adoni5 <roryjmunro1@gmail.com>
2020-05-28 12:12:54 +0100

    Added get and set flowcell and barcode functions from their own files, which were deleted, switched them to use the Session storage rather than inputs

--- web/static/web/js/utils/selectedFlowcell.js
+++
@@ -1,5 +0,0 @@
-function get_selected_flowcell() {
-
-    var input = document.querySelector('#flowcell-id');
-    return input.value;
-}
--- web/static/web/minotour_utils.js
+++ web/static/web/minotour_utils.js
@@ -8,6 +8,41 @@ Highcharts.setOptions({
 });
 
 /**
+ * Set the Flowcell ID in the session storage for whatever flowcell we are inspecting.
+ * @param flowcell_id
+ */
+function setSelectedFlowcell(flowcellId) {
+    sessionStorage.setItem("flowcellId", flowcellId);
+}
+
+/**
+ * Return the selected Flowcell from the hidden input.
+ */
+function getSelectedFlowcell() {
+    return sessionStorage.getItem("flowcellId")
+}
+
+
+/**
+ * Return the barcode name for given tab from session storage.
+ * @param tab {string} Tab name
+ * @returns {string} Barcode name for this tab.
+ */
+function getSelectedBarcode(tab) {
+    return sessionStorage.getItem(`${tab}-barcode`)
+}
+
+/**
+ * Set the barcode of the tab the user is on in the session storage.
+ * @param barcode {string} Barcode name
+ * @param tab {string} Tab name
+ */
+function setSelectedBarcode(barcode, tab) {
+    sessionStorage.setItem(`${tab}-barcode`, barcode);
+}
+
+
+/**
  * Remove old series from a given chart
  * @param chart {object}
  */
