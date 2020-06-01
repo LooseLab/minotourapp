e455cbcb7299b90c110a8522e7887cf86ed40887
adoni5 <roryjmunro1@gmail.com>
2020-06-01 12:07:32 +0100

    Black formatted files, rewrote to reflect the new imports

--- reads/tasks/tasks_update_run_summary.py
+++ reads/tasks/tasks_update_run_summary.py
@@ -8,7 +8,7 @@ from reads.models import FastqRead, Flowcell, RunSummary
 
 logger = get_task_logger(__name__)
 
-
+# TODO earmark for rewrite, it is slow
 @task()
 def update_run_summary():
     """
@@ -23,7 +23,11 @@ def update_run_summary():
 
     for flowcell in flowcell_list:
         # For all the runs under that flowcell
-        logger.info("Flowcell id {} - Updating run summaries for flowcell {}".format(flowcell.id, flowcell.id))
+        logger.info(
+            "Flowcell id {} - Updating run summaries for flowcell {}".format(
+                flowcell.id, flowcell.id
+            )
+        )
 
         for run in flowcell.runs.all():
 
@@ -37,10 +41,11 @@ def update_run_summary():
                 # If there isn't a run summary object create one
                 else:
                     logger.info(
-                        "Flowcell id {} - ELSE No objects in database creating new ones".format(flowcell.id)
+                        "Flowcell id {} - ELSE No objects in database creating new ones".format(
+                            flowcell.id
+                        )
                     )
                     run_summary = RunSummary.objects.create(
-
                         run=run,
                         read_count=None,
                         total_read_length=None,
@@ -53,10 +58,11 @@ def update_run_summary():
             # Except No run summary found. One of these code blocks is unnecessary
             except RunSummary.DoesNotExist:
                 logger.info(
-                    "Flowcell id {} - EXCEPTION No objects in database creating new ones".format(flowcell.id)
+                    "Flowcell id {} - EXCEPTION No objects in database creating new ones".format(
+                        flowcell.id
+                    )
                 )
                 run_summary = RunSummary.objects.create(
-
                     run=run,
                     read_count=None,
                     total_read_length=None,
@@ -73,88 +79,101 @@ def update_run_summary():
                 run_summary.running = True
 
                 run_summary.save()
-                reads = FastqRead.objects.filter(run=run).filter(id__gt=int(run_summary.last_read))[:20000]
+                reads = FastqRead.objects.filter(run=run).filter(
+                    id__gt=int(run_summary.last_read)
+                )[:20000]
 
                 if reads.count() > 0:
                     logger.info(
-                        "Flowcell id {} - New reads seen for run summary".format(flowcell.id)
+                        "Flowcell id {} - New reads seen for run summary".format(
+                            flowcell.id
+                        )
                     )
                     # Do the calculation
                     result = reads.aggregate(
-                        Count('sequence_length'),
-                        Min('sequence_length'),
-                        Max('sequence_length'),
-                        Sum('sequence_length'),
-                        #Avg('sequence_length'),
-                        Min('start_time'),
-                        Max('start_time'),
-                        Max('id'),
+                        Count("sequence_length"),
+                        Min("sequence_length"),
+                        Max("sequence_length"),
+                        Sum("sequence_length"),
+                        # Avg('sequence_length'),
+                        Min("start_time"),
+                        Max("start_time"),
+                        Max("id"),
                     )
                     # If there is a previously set read count, so this is not the first iteration
                     if run_summary.read_count != None:
                         logger.info(
-                            "Flowcell id {} - Previous results seen for run summary".format(flowcell.id)
+                            "Flowcell id {} - Previous results seen for run summary".format(
+                                flowcell.id
+                            )
                         )
                         # Sum the read counts
-                        run_summary.read_count += result['sequence_length__count']
+                        run_summary.read_count += result["sequence_length__count"]
                         # Sum the total lengths
-                        run_summary.total_read_length += result['sequence_length__sum']
+                        run_summary.total_read_length += result["sequence_length__sum"]
                         # Get whether the new batch has a read, save it if so or keep old value if not
-                        run_summary.max_read_length = max(run_summary.max_read_length, result['sequence_length__max'])
+                        run_summary.max_read_length = max(
+                            run_summary.max_read_length, result["sequence_length__max"]
+                        )
                         # Get whether the new batch a smaller read, save it if so or keep old value if not
-                        run_summary.min_read_length = min(run_summary.min_read_length, result['sequence_length__min'])
+                        run_summary.min_read_length = min(
+                            run_summary.min_read_length, result["sequence_length__min"]
+                        )
 
                         if len(run.RunDetails.all()):
                             run.start_time = run.RunDetails.last().minKNOW_start_time
-                            origin = 'Live data'
+                            origin = "Live data"
                             run.save()
                         else:
-                            run.start_time = min(run_summary.first_read_start_time,
-                                                                result['start_time__min'])
+                            run.start_time = min(
+                                run_summary.first_read_start_time,
+                                result["start_time__min"],
+                            )
                             run.save()
                         # run_summary.avg_read_length = result['sequence_length__avg']
                         # Get the date of the first read
-                        run_summary.first_read_start_time = min(run_summary.first_read_start_time,
-                                                                result['start_time__min'])
+                        run_summary.first_read_start_time = min(
+                            run_summary.first_read_start_time, result["start_time__min"]
+                        )
                         # Get the last read date
-                        run_summary.last_read_start_time = max(run_summary.last_read_start_time,
-                                                               result['start_time__max'])
+                        run_summary.last_read_start_time = max(
+                            run_summary.last_read_start_time, result["start_time__max"]
+                        )
                     else:
                         # If this is the first iteration no need to worry about old values, just set the fields
                         # gets turnt up
                         logger.info(
-                            "Flowcell id {} - NO Previous results seen for run summary, setting first values"
-                            .format(flowcell.id)
+                            "Flowcell id {} - NO Previous results seen for run summary, setting first values".format(
+                                flowcell.id
+                            )
                         )
-                        run_summary.read_count = result['sequence_length__count']
+                        run_summary.read_count = result["sequence_length__count"]
 
-                        run_summary.total_read_length = result['sequence_length__sum']
+                        run_summary.total_read_length = result["sequence_length__sum"]
 
-                        run_summary.max_read_length = result['sequence_length__max']
+                        run_summary.max_read_length = result["sequence_length__max"]
 
-                        run_summary.min_read_length = result['sequence_length__min']
+                        run_summary.min_read_length = result["sequence_length__min"]
                         # run_summary.avg_read_length = result['sequence_length__avg']
-                        run_summary.first_read_start_time = result['start_time__min']
-
+                        run_summary.first_read_start_time = result["start_time__min"]
 
                         if len(run.RunDetails.all()):
                             run.start_time = run.RunDetails.last().minKNOW_start_time
-                            origin = 'Live data'
+                            origin = "Live data"
                             run.save()
                         else:
-                            run.start_time = result['start_time__min']
-                            origin = 'Bsecalled Data'
+                            run.start_time = result["start_time__min"]
+                            origin = "Bsecalled Data"
                             run.save()
 
-
-
-                        run_summary.last_read_start_time = result['start_time__max']
+                        run_summary.last_read_start_time = result["start_time__max"]
                     # Get the largest id in your set of reads, set it as a new last read
-                    run_summary.last_read = result['id__max']
+                    run_summary.last_read = result["id__max"]
                     # Get the avg_read_length
-                    run_summary.avg_read_length = round(run_summary.total_read_length/run_summary.read_count)
+                    run_summary.avg_read_length = round(
+                        run_summary.total_read_length / run_summary.read_count
+                    )
 
                 run_summary.running = False
                 # save the results
                 run_summary.save()
-
