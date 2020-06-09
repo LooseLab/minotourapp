6225775780a086a748d2de44d9ca6db79eda14e6
adoni5 <roryjmunro1@gmail.com>
2020-06-09 11:09:39 +0100

    Optimise imports, black format file, added ciew to return the artic results file

--- artic/views.py
+++ artic/views.py
@@ -1,6 +1,11 @@
+import json
+import os
+import tarfile
 from collections import defaultdict
 from pathlib import Path
+from urllib.parse import parse_qs
 
+from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, render
 from rest_framework import status
 from rest_framework.decorators import api_view, renderer_classes
@@ -48,11 +53,9 @@ def quick_get_artic_results_directory(flowcell_id, barcodeName="", check=False):
         return artic_results_path.exists()
 
     if barcodeName:
-        # TODO Note this chromsome is hard coded here because we are only doing this on Covid at the moment
-        chromosome = "NC_045512.2"
         # The path to the coverage array for this barcode
         coverage_path = Path(
-            f"{artic_results_path}/{barcodeName}/coverage_{chromosome}_{flowcell_id}_{artic_task.id}_{barcodeName}.dat"
+            f"{artic_results_path}/{barcodeName}/{barcodeName}.coverage.dat"
         )
         return flowcell, artic_results_path, artic_task.id, coverage_path
 
@@ -332,11 +335,11 @@ def get_artic_column_chart_data(request):
         return_data = defaultdict(list)
         queryset = PafSummaryCov.objects.filter(
             job_master__id=artic_task.id
-        ).values_list("barcode_name", "average_read_length", "coverage")
-        for barcode, read_length, coverage in queryset:
+        ).values_list("barcode_name", "average_read_length", "read_count")
+        for barcode, read_length, read_count in queryset:
             return_data["barcodes"].append(barcode)
             return_data["average_read_length"].append(read_length)
-            return_data["coverage"].append(coverage)
+            return_data["read_counts"].append(read_count)
 
     except TypeError as e:
         print(e.args)
@@ -402,9 +405,7 @@ def get_artic_summary_table_data(request):
         pafsummarycov["percent_250x"] = artic_metadata[
             barcode_name
         ].percentage_bases_over_250x
-        pafsummarycov["has_finished"] = artic_metadata[
-            barcode_name
-        ].has_finished
+        pafsummarycov["has_finished"] = artic_metadata[barcode_name].has_finished
         pafsummarycov["has_sufficient_coverage"] = artic_metadata[
             barcode_name
         ].has_sufficient_coverage
@@ -456,7 +457,7 @@ def get_artic_barcode_metadata_html(request):
             for all_number in all_numbers:
                 total_reads += all_number
             orm_object.percentage_of_reads_in_barcode = (
-                barcode_numbers.read_count / total_reads * 100
+                    barcode_numbers.read_count / total_reads * 100
             )
 
         except FlowcellSummaryBarcode.DoesNotExist as e:
@@ -474,14 +475,24 @@ def get_artic_barcode_metadata_html(request):
         ["Has Finished", "has_finished"],
         ["Has Sufficient Coverage", "has_sufficient_coverage"],
     ]
+    results_files = [
+        ("Consensus sequence", "consensus"),
+        ("Box plot", "box-plot"),
+        ("Bar plot", "bar-plot"),
+        ("Fail VCF", "fail-vcf"),
+        ("Pass VCF", "pass-vcf"),
+        ("Input fasta", "input-fasta"),
+    ]
     old_dict = orm_object.__dict__
     context_dict = {key[0]: old_dict[key[1]] for key in new_key_names}
     context_dict["barcode_name"] = orm_object.barcode.name
     context_dict["flowcell_id"] = flowcell_id
+
     _, path_to_results, _, _ = quick_get_artic_results_directory(
         flowcell_id, selected_barcode
     )
-    context_dict["path_to_results"] = path_to_results / "concensus.fasta"
+    context_dict["path_to_results"] = path_to_results / "results.tar.gz"
+    context_dict["results_files"] = results_files
 
     return render(
         request,
@@ -508,14 +519,21 @@ def manually_create_artic_command_job_master(request):
     flowcell_id = request.data.get("flowcellId", None)
     reference_name = "covid_19"
     if not job_type_id or not barcode_name or not flowcell_id:
-        return Response("Flowcell id, barcode id, job_type_id are required fields.", status=status.HTTP_400_BAD_REQUEST)
+        return Response(
+            "Flowcell id, barcode id, job_type_id are required fields.",
+            status=status.HTTP_400_BAD_REQUEST,
+        )
     try:
         reference = ReferenceInfo.objects.get(name="covid_19")
-        barcode = Barcode.objects.filter(name=barcode_name, run__in=Flowcell.objects.get(pk=flowcell_id).runs.all()).last()
+        barcode = Barcode.objects.filter(
+            name=barcode_name, run__in=Flowcell.objects.get(pk=flowcell_id).runs.all()
+        ).last()
 
     except [ReferenceInfo.DoesNotExist, ReferenceInfo.MultipleObjectsReturned] as e:
-        return Response(f"Task was expecting reference of name {reference_name}, and only one. Exception: {e}",
-                        status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        return Response(
+            f"Task was expecting reference of name {reference_name}, and only one. Exception: {e}",
+            status=status.HTTP_500_INTERNAL_SERVER_ERROR,
+        )
     # TODO potential bug here where the barcode name on the reads is not the barcode name we save
     job_master, created = JobMaster.objects.get_or_create(
         job_type_id=job_type_id,
@@ -524,6 +542,73 @@ def manually_create_artic_command_job_master(request):
         flowcell_id=flowcell_id,
     )
     if created:
-        return Response("Successfully created task to be consumed by Celery later.", status=status.HTTP_200_OK)
+        return Response(
+            "Successfully created task to be consumed by Celery later.",
+            status=status.HTTP_200_OK,
+        )
     else:
-        return Response("Already a JobMaster for this barcode on this flowcell.", status=status.HTTP_403_FORBIDDEN)
+        return Response(
+            "Already a JobMaster for this barcode on this flowcell.",
+            status=status.HTTP_403_FORBIDDEN,
+        )
+
+
+@api_view(["GET"])
+def get_results_package(request):
+    """
+    TODO clear results after download
+    Create a results package for artic barcode data, for user download. Need a way to clear it. Maybe a timed call? A celery task?
+    Parameters
+    ----------
+    request: rest_framework.request.Request
+        Post request data. Contains files chosen, flowcell id and barcode.
+    Returns
+    -------
+        Status
+
+    """
+    params = json.loads(request.GET.get("params", None))
+
+    flowcell_id = params.get("flowcellId", None)
+    barcode = params.get("selectedBarcode", None)
+    chosen = parse_qs(request.GET.get("string"))
+    if not flowcell_id or not barcode:
+        return Response("Flowcell Id or barocde required in request.", status=status.HTTP_400_BAD_REQUEST)
+
+    try:
+        flowcell = Flowcell.objects.get(pk=flowcell_id)
+    except Flowcell.DoesNotExist:
+        return Response("Flowcell not found.", status=status.HTTP_404_NOT_FOUND)
+
+    # TODO add check to see if task is complete
+    print(request.data)
+    _, path_to_results, _, _ = quick_get_artic_results_directory(
+        flowcell_id, barcode
+    )
+    results_files = {
+        "consensus":f"{barcode}.consensus.fasta.gz",
+        "box-plot": f"{barcode}-boxplot.png.gz",
+        "bar-plot": f"{barcode}-barplot.png.gz",
+        "fail-vcf": f"{barcode}.fail.vcf.gz",
+        "pass-vcf": f"{barcode}.pass.vcf.gz",
+        "input-fasta": f"{barcode}.fastq.gz"
+    }
+    chosen_files = [results_files[key] for key in chosen.keys()]
+    # change into the directory
+    os.chdir(path_to_results / barcode)
+    results_file = path_to_results / barcode / f"results_artic_{flowcell.name}.tar.gz"
+
+    with tarfile.open(results_file, "w:gz") as tar:
+        try:
+            for filey in chosen_files:
+                tar.add(filey)
+        except FileNotFoundError as e:
+            return Response(str(e), status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+    with open(results_file, "rb") as fh:
+        response = HttpResponse(fh.read(), content_type="application/gzip")
+        response["Content-Disposition"] = f"attachment; filename=results_artic_{flowcell.name}.tar.gz"
+        response["x-file-name"] = f"results_artic_{flowcell.name}.tar.gz"
+        return response
+
+
