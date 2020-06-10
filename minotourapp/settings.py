3df355f4934e085cdef2daee0860217a26156bfc
adoni5 <roryjmunro1@gmail.com>
2020-06-10 19:26:11 +0100

    remove unused comment line

--- minotourapp/settings.py
+++ minotourapp/settings.py
@@ -143,7 +143,6 @@ USE_L10N = True
 USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, '../minotour_static/')
