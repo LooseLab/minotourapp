67e8280824668624ce5f868aa7b8beec94111af9
adoni5 <roryjmunro1@gmail.com>
2020-06-22 20:13:47 +0100

    Added in api/v1/communication pattern. Added in check for coverageapi endpoint to see if there is an alignment task.

--- communication/urls.py
+++ communication/urls.py
@@ -1,10 +1,15 @@
 from django.conf.urls import url
+
 from communication import views
 
 urlpatterns = [
     url(
-        r'^api/v1/messages/conditions$',
+        r'^api/v1/communication/messages/conditions$',
         views.get_create_delete_conditions,
         name="conditions"
-    ),
+    ), url(
+        r'^api/v1/communication/(?P<pk>[0-9]+)/chromosomecoverage/$',
+        views.get_coverage_summary,
+        name="get_coverage_summary"
+    )
 ]
