0071aadb22dc21092ed44d824dbc167ee347d553
adoni5 <roryjmunro1@gmail.com>
2020-06-22 20:06:05 +0100

    Made messages have a nice fade in fade out. Otherwise this code is garbage

--- web/static/web/js/views/MView.js
+++ web/static/web/js/views/MView.js
@@ -1,15 +1,18 @@
 class MView {
-    /*
+  /*
     Create the view with the element as a an argument - the element is a HTML element
      */
-    constructor(elemento) {
+  constructor (elemento) {
+    this._elemento = elemento
+  }
 
-        this._elemento = elemento;
-    }
-
-    update(model) {
-        // set the element inner html to the passed html
-        this._elemento.innerHTML = this.template(model);
-        setTimeout(()=>{this._elemento.innerHTML=""}, 3000);
-    }
+  update (model) {
+    // set the element inner html to the passed html#
+    const garboCode = $(this._elemento)
+    this._elemento.innerHTML = this.template(model)
+    garboCode.fadeIn()
+    setTimeout(() => {
+      garboCode.fadeOut()
+    }, 3000)
+  }
 }
