586170b4fd3e7873fa9c541dbf0bdaa416a1a9ff
adoni5 <roryjmunro1@gmail.com>
2020-06-22 20:02:38 +0100

    Eslinted file. Rewrote to update page content when the tab is switched to.

--- web/static/web/js/controllers/FlowcellTabController.js
+++ web/static/web/js/controllers/FlowcellTabController.js
@@ -1,117 +1,122 @@
 class FlowcellTabController {
-    /*
+  /*
     Once a flowcell has been selected, this class is constructed to take charge of the tab switching
      */
-    constructor(flowcellId) {
-        this._flowcellId = flowcellId;
-        this._flowcellServices = new FlowcellService();
-        this._drawTabs();
-        this.tabs = [];
-        this._first = true;
-        this._createElementLookupObjects();
-        this._redraw_interval = setInterval(() => this._drawTabs(), 30000);
+  constructor (flowcellId) {
+    this._flowcellId = flowcellId
+    this._flowcellServices = new FlowcellService()
+    this._drawTabs()
+    this.tabs = []
+    this._first = true
+    this._createElementLookupObjects()
+    this._redraw_interval = setInterval(() => this._drawTabs(), 30000)
+  }
+
+  /**
+   * Create a object that has all the tabs and nav HTML objects keyed to the elements ID attribute
+   */
+  _createElementLookupObjects () {
+    const that = this
+    this.lookupElement = {}
+    $(`.flowcell-tab-ctl`).each((i, obj) => {
+      that.lookupElement[obj.id] = obj
+    })
+  }
+
+  /**
+   * Draw the tabs for the first time on the flowcell index.html page, then show one tab of data
+   */
+  _drawTabs () {
+    const promise = this._flowcellServices.getFlowcellTabs(this._flowcellId)
+    promise.then((tabs) => {
+      const seshTab = getSelectedTab()
+      // We are getting the previous tab here and checking it still is available. If it is we show it.
+      this.showTabs(tabs)
+      this.tabs = tabs
+      if (this._first) {
+        const activeTab = seshTab !== null && tabs.includes(seshTab) ? seshTab : `summary-data`
+        this.toggleTabContent(activeTab)
+        this._first = false
+      }
+    })
+  }
+
+  /**
+   * Toggle the content to show the tab that the user has selected.
+   * @param name {string} The name of the seleceted tab.
+   */
+  toggleTabContent (name) {
+    const navName = `nav-${name}`
+    const tabName = `tab-${name}`
+    let oldNavTab, oldTabContent, controllerName
+    const lookupController = {
+      "sequence-mapping": `alignmentController`,
+      "basecalled-data": `baseCalledDataController`,
+      artic: `articController`
     }
 
-
-    /**
-     * Create a object that has all the tabs and nav HTML objects keyed to the elements ID attribute
-     */
-    _createElementLookupObjects() {
-        let that = this
-        this.lookupElement = {}
-        $(".flowcell-tab-ctl").each((i, obj) => {
-            that.lookupElement[obj.id] = obj;
-        });
+    if (getSelectedTab()) {
+      oldNavTab = `nav-${getSelectedTab()}`
+      oldTabContent = `tab-${getSelectedTab()}`
+      // remove active class from current nav tab child element, the link
+      // Add hidden tab to old tab content
+      this.lookupElement[oldTabContent].classList.add(`hidden`)
+      this.lookupElement[oldNavTab].firstElementChild.classList.remove(`active`)
     }
-
-    /**
-     * Draw the tabs for the first time on the flowcell index.html page, then show one tab of data
-     */
-    _drawTabs() {
-        let promise = this._flowcellServices.getFlowcellTabs(this._flowcellId);
-        promise.then((tabs) => {
-            let seshTab = getSelectedTab();
-            // We are getting the previous tab here and checking it still is available. If it is we show it.
-            this.showTabs(tabs);
-            this.tabs = tabs;
-            if (this._first) {
-
-                let activeTab = seshTab !== null && tabs.includes(seshTab) ? seshTab : "summary-data";
-                this.toggleTabContent(activeTab);
-                this._first = false;
-            }
-        });
+    // set the new tab in Session Storage.
+    setSelectedTab(name)
+    // add active to newly selected nav time child element, the link
+    this.lookupElement[navName].firstElementChild.classList.add(`active`)
+    // show the content of the newly selected Div
+    this.toggleContent(this.lookupElement[navName], this.lookupElement[tabName])
+    controllerName = lookupController[name]
+    if (Object.prototype.hasOwnProperty.call(this, controllerName)) {
+      console.log(`Updating tab ${name}`)
+      this[controllerName].updateTab()
     }
+  }
 
-    /**
-     * Toggle the content to show the tab that the user has selected.
-     * @param name {string} The name of the seleceted tab.
-     */
-    toggleTabContent(name) {
+  /**
+   * Show the content of the select nav-tab in the tab content div.
+   * @param nav {object} HTML object representing the nav tab to be labelled as active.
+   * @param tab {object} HTML object representing the tab content to be shown.
+   */
+  toggleContent (nav, tab) {
+    // As of bootstrap 4 we need to add the active class to the a link not the Li element
+    nav.children[0].classList.add(`active`)
+    tab.classList.remove(`hidden`)
+    tab.classList.add(`active`)
+    setSelectedTab(tab.id.substr(4))
+    // Bad call to request data
+    app.requestData(this._flowcellId)
+  }
 
-        let navName = `nav-${name}`;
-        let tabName = `tab-${name}`;
-        let oldNavTab, oldTabContent;
-        if (getSelectedTab()) {
-            oldNavTab = `nav-${getSelectedTab()}`;
-            oldTabContent = `tab-${getSelectedTab()}`;
-            // remove active class from current nav tab child element, the link
-            // Add hidden tab to old tab content
-            this.lookupElement[oldTabContent].classList.add("hidden");
-            this.lookupElement[oldNavTab].firstElementChild.classList.remove("active");
+  /**
+   * Show the nav tabs on the flowcell_index.html page by removing the hidden class on them.
+   * @param tabs {string[]} Array of tab names
+   */
+  showTabs (tabs) {
+    const controllers = {
+      "basecalled-data": [`baseCalledDataController`, BasecalledDataController],
+      artic: [`articController`, ArticController],
+      "sequence-mapping": [`alignmentController`, AlignmentController]
+    }
+    Object.keys(controllers).forEach(controllerName => {
+      // if we have a controller for this tab, but we are no longer showing it as underlying data has been deleted
+      if (!tabs.includes(controllerName) && this[controllers[controllerName][0]]) {
+        // delete the controller
+        delete this[controllers[controllerName][0]]
+      }
+    })
+    tabs.forEach((name) => {
+      this.lookupElement[`nav-${name}`].classList.remove(`hidden`)
+      // if the name of the tab is in our list of controllers to initialise
+      if (Object.keys(controllers).includes(name)) {
+        // if we don't have a controller already initialised for this tab
+        if (!this[controllers[name][0]]) {
+          this[controllers[name][0]] = new controllers[name][1](this._flowcellId)
         }
-
-        // set the new tab in Session Storage.
-        setSelectedTab(name);
-
-        // add active to newly selected nav time child element, the link
-        this.lookupElement[navName].firstElementChild.classList.add("active");
-
-        // show the content of the newly selected Div
-        this.toggleContent(this.lookupElement[navName], this.lookupElement[tabName]);
-    }
-
-    /**
-     * Show the content of the select nav-tab in the tab content div.
-     * @param nav {object} HTML object representing the nav tab to be labelled as active.
-     * @param tab {object} HTML object representing the tab content to be shown.
-     */
-    toggleContent(nav, tab) {
-        // As of bootstrap 4 we need to add the active class to the a link not the Li element
-        nav.children[0].classList.add("active");
-        tab.classList.remove("hidden");
-        tab.classList.add("active");
-        setSelectedTab(tab.id.substr(4));
-        // Bad call to request data
-        app.requestData(this._flowcellId);
-    }
-
-    /**
-     * Show the nav tabs on the flowcell_index.html page by removing the hidden class on them.
-     * @param tabs {string[]} Array of tab names
-     */
-    showTabs(tabs) {
-        let controllers = {
-            "basecalled-data": ["baseCalledDataController", BasecalledDataController],
-            "artic": ["articController", ArticController],
-            "sequence-mapping": ["alignmentController", AlignmentController]
-        };
-        Object.keys(controllers).forEach(controllerName=>{
-            // if we have a controller for this tab, but we are no longer showing it as underlying data has been deleted
-            if (!tabs.includes(controllerName) && this[controllers[controllerName][0]]){
-                // delete the controller
-                delete this[controllers[controllerName][0]]
-            }
-        })
-        tabs.forEach((name) => {
-            this.lookupElement[`nav-${name}`].classList.remove("hidden");
-            // if the name of the tab is in our list of controllers to initialise
-            if ( Object.keys(controllers).includes(name)) {
-                // if we don't have a controller already initialised for this tab
-                if (!this[controllers[name][0]]){
-                    this[controllers[name][0]] = new controllers[name][1](this._flowcellId);
-                }
-            }
-        });
-    }
+      }
+    })
+  }
 }
