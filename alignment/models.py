7728b2bc6d2317d4cef9e10475e4bbc49c06176a
adoni5 <roryjmunro1@gmail.com>
2020-06-22 20:05:09 +0100

    Deleted PafStore model. Added fields to PafRoughCov - for binning purposes

--- alignment/models.py
+++ alignment/models.py
@@ -1,63 +1,7 @@
 from django.db import models
-from django.db.models.signals import post_save
-from django.dispatch import receiver
 
 from reads.models import Barcode, FastqRead, FastqReadType, Run, GroupRun, Flowcell, JobMaster
 from reference.models import ReferenceInfo, ReferenceLine
-
-
-class PafStore(models.Model):
-
-    job_master = models.ForeignKey(
-        JobMaster, on_delete=models.CASCADE, related_name="pafstore_list"
-    )
-
-    flowcell = models.ForeignKey(  # TODO delete - read from job_master.flowcell
-        Flowcell,
-        on_delete=models.CASCADE,
-        related_name="flowcellpafalignemnts",
-        null=True,
-        blank=True,
-    )
-
-    read = models.ForeignKey(
-        FastqRead, related_name="pafreadalignment", on_delete=models.CASCADE,
-    )
-
-    reference = models.ForeignKey(  # TODO delete - read from job_master.reference
-        ReferenceInfo, related_name="pafstorereference", on_delete=models.CASCADE,
-    )
-
-    # PAF File Format:
-
-    qsn = models.CharField(max_length=256)  # 1	string	Query sequence name
-
-    qsl = models.IntegerField()  # 2	int	Query sequence length
-
-    qs = models.IntegerField()  # 3	int	Query start (0-based)
-
-    qe = models.IntegerField()  # 4	int	Query end (0-based)
-
-    rs = models.CharField(max_length=1)  # 5	char	Relative strand: "+" or "-"
-
-    tsn = models.ForeignKey(
-        ReferenceLine, on_delete=models.CASCADE, related_name="pafstore_list"
-    )  # 6	Foreign key Target sequence name
-
-    tsl = models.IntegerField()  # 7	int	Target sequence length
-
-    ts = models.IntegerField()  # 8	int	Target start on original strand (0-based)
-
-    te = models.IntegerField()  # 9	int	Target end on original strand (0-based)
-
-    nrm = models.IntegerField()  # 10	int	Number of residue matches
-
-    abl = models.IntegerField()  # 11	int	Alignment block length
-
-    mq = models.IntegerField()  # 12	int	Mapping quality (0-255; 255 for missing)
-
-    def __str__(self):
-        return "{}".format(self.qsn)
 
 
 class PafRoughCov(models.Model):
@@ -96,8 +40,6 @@ class PafRoughCov(models.Model):
         Barcode, related_name="prc_barcode", null=True, on_delete=models.DO_NOTHING,
     )
 
-    barcode_name = models.CharField(max_length=32)
-
     reference = models.ForeignKey(
         ReferenceInfo, related_name="pafreference", on_delete=models.CASCADE,
     )
@@ -106,16 +48,14 @@ class PafRoughCov(models.Model):
         ReferenceLine, related_name="pafchromosome", on_delete=models.CASCADE,
     )
 
-    p = models.IntegerField()  # position
+    bin_position_start = models.IntegerField(default=0)  # position
 
-    i = models.IntegerField(default=0)  # incdel
+    bin_position_end = models.IntegerField(default=0)
 
-    def to_chart_data(self):
-
-        return [self.p, self.i]
+    bin_coverage = models.IntegerField(default=0)
 
     def __str__(self):
-        return "{} {}".format(self.flowcell, self.p)
+        return f"{self.flowcell} {self.p}"
 
 
 class PafSummaryCov(models.Model):
@@ -127,6 +67,13 @@ class PafSummaryCov(models.Model):
         JobMaster, on_delete=models.CASCADE, related_name="paf_summary_cov_list"
     )
 
+    barcode = models.ForeignKey(
+        Barcode,
+        on_delete=models.CASCADE,
+        related_name="barcode_paf_summaries",
+        null=True
+    )
+
     barcode_name = models.CharField(max_length=32)
 
     chromosome = models.ForeignKey(
@@ -135,12 +82,12 @@ class PafSummaryCov(models.Model):
         on_delete=models.CASCADE,
         null=True,
     )
-
+    # TODO to be deleted, use field from chromosome
     reference_line_length = models.BigIntegerField()
 
     read_count = models.BigIntegerField(default=0)
 
-    total_length = models.BigIntegerField(default=0)
+    total_yield = models.BigIntegerField(default=0)
 
     read_type = models.ForeignKey(
         FastqReadType,
@@ -156,50 +103,3 @@ class PafSummaryCov(models.Model):
     average_read_length = models.IntegerField(
         default=0
     )
-
-
-@receiver(post_save, sender=PafStore)
-def updatePafRoughCov(instance, sender, **kwargs):
-    pafline = instance
-    if pafline.run is not None:
-        pafstart, created1 = PafRoughCov.objects.update_or_create(
-            run=pafline.run,
-            read_type=pafline.read.type,
-            barcode=pafline.read.barcode,
-            reference=pafline.reference,
-            chromosome=pafline.tsn,
-            p=pafline.ts,  # position
-        )
-        pafstart.i += 1
-        pafstart.save()
-        pafend, created2 = PafRoughCov.objects.update_or_create(
-            run=pafline.run,
-            read_type=pafline.read.type,
-            barcode=pafline.read.barcode,
-            reference=pafline.reference,
-            chromosome=pafline.tsn,
-            p=(pafline.te) + 1,  # position
-        )
-        pafend.i -= 1
-        pafend.save()
-    else:
-        pafstart, created1 = PafRoughCov.objects.update_or_create(
-            flowcell=pafline.flowcell,
-            read_type=pafline.read.type,
-            barcode=pafline.read.barcode,
-            reference=pafline.reference,
-            chromosome=pafline.tsn,
-            p=pafline.ts,  # position
-        )
-        pafstart.i += 1
-        pafstart.save()
-        pafend, created2 = PafRoughCov.objects.update_or_create(
-            flowcell=pafline.flowcell,
-            read_type=pafline.read.type,
-            barcode=pafline.read.barcode,
-            reference=pafline.reference,
-            chromosome=pafline.tsn,
-            p=(pafline.te) + 1,  # position
-        )
-        pafend.i -= 1
-        pafend.save()
