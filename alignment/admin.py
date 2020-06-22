e63134aaddd382e93f0a9073999c32a30b62b307
adoni5 <roryjmunro1@gmail.com>
2020-06-22 19:50:10 +0100

    Remove PafStore from admin registration

--- alignment/admin.py
+++ alignment/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 
-from .models import PafRoughCov, PafStore, PafSummaryCov
+from .models import PafRoughCov, PafSummaryCov
 
-admin.site.register(PafStore)
 admin.site.register(PafRoughCov)
 admin.site.register(PafSummaryCov)
