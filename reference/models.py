e366658b682df06b3a37d4dd272815af7a72f187
adoni5 <roryjmunro1@gmail.com>
2020-06-29 11:26:35 +0100

    Added file fields to the ReferenceInfo model for index

--- reference/models.py
+++ reference/models.py
@@ -10,47 +10,43 @@ class ReferenceInfo(models.Model):
     """
 
     name = models.CharField(
-
-        max_length=512
+        max_length=256
     )
-
     filename = models.CharField(
-
-        max_length=512
+        max_length=256
     )
-
-    minimap2_index_file_location = models.CharField(
-
-        max_length=512,
+    minimap2_index_file_location = models.FileField(
+        upload_to="minimap2_indexes/",
+        null=True,
+        blank=True
+    )
+    file_location = models.FileField(
+        upload_to="reference_files",
+        default="",
         null=True
     )
-
     length = models.BigIntegerField(
-
         default=0
     )
-
     private = models.BooleanField(
-
         default=False
     )
-
     owner = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         related_name='user_references',
         on_delete=models.CASCADE,
         null=True
     )
-
     users = models.ManyToManyField(
-
         settings.AUTH_USER_MODEL,
         blank=True
     )
-
     transcripts = models.BooleanField(
-
         default=False
+    )
+    md5_checksum = models.CharField(
+        max_length=256,
+        default=""
     )
 
     def __str__(self):
@@ -58,20 +54,18 @@ class ReferenceInfo(models.Model):
 
 
 class ReferenceLine(models.Model):
-
+    """
+    Represents contigs inside the reference
+    """
     reference = models.ForeignKey(
         ReferenceInfo,
         on_delete=models.CASCADE,
         related_name='referencelines'
     )
-
     line_name = models.CharField(
         max_length=512
     )
-
     chromosome_length = models.BigIntegerField(
-
     )
-
     def __str__(self):
         return "{} {}".format(self.reference, self.line_name)
