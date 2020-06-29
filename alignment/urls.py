f5a25aa72deb768fe908837ec09731d12badfb5b
adoni5 <roryjmunro1@gmail.com>
2020-06-29 11:31:47 +0100

    Added end point to get per chromosome coverage

--- alignment/urls.py
+++ alignment/urls.py
@@ -4,7 +4,7 @@ from alignment import views
 
 urlpatterns = [
     url(
-        r'^api/v1/alignment/coverage/(?P<task_id>[0-9]+)/(?P<barcode_name>[a-zA-Z0-9 ]+)/(?P<read_type_id>[0-9]+)/(?P<chromosome_id>[0-9]+)/$',
+        r'^api/v1/alignment/coverage/(?P<task_id>[0-9]+)/(?P<barcode_id>[a-zA-Z0-9 ]+)/(?P<read_type_id>[0-9]+)/(?P<chromosome_id>[0-9]+)/$',
         views.rough_coverage_complete_chromosome_flowcell,
         name="rough_coverage_complete_chromosome_flowcell"
     ),
@@ -20,8 +20,13 @@ urlpatterns = [
     ),
 
     url(
-        r'^api/v1/alignment/(?P<pk>[0-9]+)/pafsummarytable/$',
+        r'^api/v1/alignment/(?P<pk>[0-9]+)/paf-summary-table/$',
         views.paf_summary_table_json,
         name="flowcell_paf_summary"
     ),
+    url(
+        r'^api/v1/alignment/(?P<flowcell_pk>[0-9]+)/genome-coverage-summary',
+        views.per_genome_coverage_summary,
+        name="genome-coverage-summary"
+    )
 ]
