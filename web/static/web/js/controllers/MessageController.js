c935f9728492ae94bca02f21bcf153f0822b967a
adoni5 <roryjmunro1@gmail.com>
2020-07-02 17:34:24 +0100

    Es linted file

--- web/static/web/js/controllers/MessageController.js
+++ web/static/web/js/controllers/MessageController.js
@@ -1,22 +1,22 @@
 class MessageController {
+  // TODO add rewrite to interval update
+  constructor () {
+    this._messageTable = null
+  }
 
-    constructor() {
-        this._messageTable = null;
-    }
-
-    drawMessageTable() {
-        console.log("drawing table");
-        $("#message-table").DataTable({
-            ajax: {
-                url: '/web/private/sent_tweets/',
-                method: "GET",
-            },
-            columns: [
-                {"data": "title"},
-                {"data": "created_date"},
-                {"data": "sender_first_name"},
-                {"data": "flowcell_name"}
-            ]
-        });
-    }
+  drawMessageTable () {
+    console.log(`drawing table`)
+    $(`#message-table`).DataTable({
+      ajax: {
+        url: `/web/private/sent_tweets/`,
+        method: `GET`
+      },
+      columns: [
+        { data: `title` },
+        { data: `created_date` },
+        { data: `sender_first_name` },
+        { data: `flowcell_name` }
+      ]
+    })
+  }
 }
