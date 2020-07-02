05bca4159d09fab94e9a37ca9962cb8128e011bc
adoni5 <roryjmunro1@gmail.com>
2020-07-02 18:49:09 +0100

    fixes to typos

--- communication/views.py
+++ communication/views.py
@@ -10,8 +10,7 @@ from alignment.models import PafSummaryCov
 from communication.models import Message
 from communication.models import NotificationConditions
 from communication.serializers import MessageSerializer, NotificationSerialiser
-from reads.models import Flowcell, Barcode, UserOptions, JobMaster
-from reference.models import ReferenceInfo, ReferenceLine
+from reads.models import JobMaster
 from .forms import NotificationConditionForm
 
 
@@ -203,112 +202,16 @@ def notification_conditions_list(request):
     -------
     rest_framework.response.Response
     """
-    condition_lookup = {
-        "coverage": "cov",
-        "newTarget": "targ",
-        "Mux alerts": "mux",
-        "Voltage": "volt",
-        "Temperature": "temp",
-        "Warnings/Errors": "waer",
-        "Occupancy": "occu",
-        "MinoTour": "mino",
-    }
 
-    if request.method == "POST":
-
-        flowcell_pk = request.data.get("flowcell", None)
-
-        flowcell = Flowcell.objects.get(pk=flowcell_pk)
-
-        conditions = request.data.get("conditions", None)
-
-        coverage_settings = request.data.get("coverage_sets", {})
-
-        auto_choices = request.data.get("autoChoices", [])
-
-        auto_values = request.data.get("rangeValues", [])
-
-        twitter_settings = UserOptions.objects.get(owner=request.user)
-
-        tweet = twitter_settings.tweet
-
-        handle = twitter_settings.twitterhandle
-
-        errors_list = []
-
-        if flowcell is None:
-            return Response("No flowcell submitted", status=400)
-        if conditions is None:
-            return Response("No conditions submitted", status=400)
-        if not tweet and bool(handle):
-            return Response(
-                "Please provide Twitter permission in the profile section, and set a handle.",
-                status=400,
-            )
-        # Create conditions for each contained in the request body
-        for condition, value in conditions.items():
-
-            cov_targ = value if condition == "coverage" else None
-
-            condition_name = condition_lookup[condition]
-
-            if condition == "coverage" and bool(coverage_settings) and value:
-
-                repeat = False
-
-                reference = ReferenceInfo.objects.get(pk=coverage_settings["reference"])
-                chromosome = ReferenceLine.objects.get(
-                    pk=coverage_settings["chromosome"]
-                )
-                kwargs = {
-                    "conditional_reference": reference,
-                    "chromosome": chromosome,
-                    "notification_type": condition_name,
-                    "repeat": repeat,
-                    "coverage_target": cov_targ,
-                }
-
-                if isinstance(coverage_settings["barcodes"], list):
-
-                    for barcode in coverage_settings["barcodes"]:
-                        kwargs["barcode"] = Barcode.objects.get(pk=barcode)
-                        cond = create_notification_conditions(
-                            flowcell, request.user, **kwargs
-                        )
-                        errors_list.append(cond)
-
-            # Mux or new target
-        for condition in auto_choices:
-            upper_limit = None
-            lower_limit = None
-            defaults = {"Occupancy": [40, 100], "Voltage": [-180, -160]}
-            if condition in ["Occupancy", "Voltage"]:
-                upper_limit = auto_values.get(condition, {}).get(
-                    "upper", defaults[condition][1]
-                )
-                lower_limit = auto_values.get(condition, {}).get(
-                    "lower", defaults[condition][0]
-                )
-            condition_name = condition_lookup[condition]
-            kwargs = {
-                "notification_type": condition_name,
-                "repeat": True,
-                "upper_limit": upper_limit,
-                "lower_limit": lower_limit,
-            }
-            cond = create_notification_conditions(flowcell, request.user, **kwargs)
-            errors_list.append(cond)
-        return_list = ["Successfully created notification conditions.", "Issues:"]
-        # Remove None, as successfully create notification conditions function returns none.
-        errors_list = list(filter(None, errors_list))
-        # If we have no errors
-        if not errors_list:
-            return_list[1] = "Issues: None."
-        return_list.extend(errors_list)
-        return_list = list(filter(None, return_list))
-        return_string = "<br/><br/>".join(return_list)
-        return Response(return_string, status=200)
-
+    if request.method == "DELETE":
+        notification_pk = request.GET.get("pk", -1)
+        try:
+            deleted = NotificationConditions.objects.get(
+                pk=int(notification_pk)
+            ).delete()
+            return Response(f"Deleted {deleted}", status=200)
+        except Exception as e:
+            return Response(e, status=500)
     elif request.method == "GET":
         flowcell_id = request.GET.get("flowcellId", -1)
         if flowcell_id == -1:
@@ -318,17 +221,6 @@ def notification_conditions_list(request):
         )
         notifications_serialiser = NotificationSerialiser(queryset, many=True)
         return Response({"data": notifications_serialiser.data}, status=200)
-
-    elif request.method == "DELETE":
-        notification_pk = request.GET.get("pk", -1)
-        try:
-            deleted = NotificationConditions.objects.get(
-                pk=int(notification_pk)
-            ).delete()
-            return Response(f"Deleted {deleted}", status=200)
-        except Exception as e:
-            return Response(e, status=500)
-
 
 @api_view(["GET"])
 def get_coverage_summary(request, pk):
