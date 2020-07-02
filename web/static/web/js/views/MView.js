9639513611fb3d83bd618d8173ee01260092aa66
adoni5 <roryjmunro1@gmail.com>
2020-07-02 17:59:52 +0100

    I hate this file

--- web/static/web/js/views/MView.js
+++ web/static/web/js/views/MView.js
@@ -2,17 +2,19 @@ class MView {
   /*
     Create the view with the element as a an argument - the element is a HTML element
      */
-  constructor (elemento) {
-    this._elemento = elemento
+  constructor (element) {
+    this._element = element
   }
 
   update (model) {
     // set the element inner html to the passed html#
-    const garboCode = $(this._elemento)
-    this._elemento.innerHTML = this.template(model)
+    const garboCode = $(this._element)
+    garboCode.css(`display`, `block`)
+    garboCode.html(this.template(model))
     garboCode.fadeIn()
     setTimeout(() => {
       garboCode.fadeOut()
+      garboCode.css(`display`, `none`)
     }, 10000)
   }
 }
