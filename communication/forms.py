3ce83a3fc6350732710bc0917d75c093993897e5
adoni5 <roryjmunro1@gmail.com>
2020-07-02 17:31:02 +0100

    Added form for Condition creation

---
+++ communication/forms.py
@@ -0,0 +1,15 @@
+"""
+Defines form handling for creating notification conditio
+"""
+from django import forms
+
+from .models import NotificationConditions
+
+
+class NotificationConditionForm(forms.ModelForm):
+    """
+    Form to save references uploaded by the user
+    """
+    class Meta:
+        model = NotificationConditions
+        fields = ("upper_limit", "lower_limit", "notification_type", "flowcell", "chromosome", "reference", "coverage_target")
