058764e0a66b2ef1391ad069301515f68ba2d64c
adoni5 <roryjmunro1@gmail.com>
2020-07-02 17:32:03 +0100

    Eslinted file

--- web/static/web/js/models/Message.js
+++ web/static/web/js/models/Message.js
@@ -1,41 +1,34 @@
 class Message {
+  constructor (title, uuidstr, recipient_email, is_read, created_date) {
+    this._title = title
+    this._uuidstr = uuidstr
+    this._recipient_email = recipient_email
+    this._is_read = is_read
+    this._created_date = created_date
+  }
 
-    constructor(title, uuidstr, recipient_email, is_read, created_date) {
+  get title () {
+    return this._title
+  }
 
-        this._title = title;
-        this._uuidstr = uuidstr;
-        this._recipient_email = recipient_email;
-        this._is_read = is_read;
-        this._created_date = created_date;
-    }
+  get uuidstr () {
+    return this._uuidstr
+  }
 
-    get title() {
+  get recipient_email () {
+    return this._recipient_email
+  }
 
-        return this._title;
-    }
+  get is_read () {
+    return this._is_read
+  }
 
-    get uuidstr() {
+  get created_date () {
+    return this._created_date
+  }
 
-        return this._uuidstr;
-    }
+  /* set text(text) {
 
-    get recipient_email() {
-
-        return this._recipient_email;
-    }
-
-    get is_read() {
-
-        return this._is_read;
-    }
-
-    get created_date() {
-
-        return this._created_date;
-    }
-
-    /*set texto(texto) {
-
-        this._texto = texto;
-    }*/
+        this._texto = text;
+    } */
 }
