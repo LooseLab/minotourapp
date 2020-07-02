85a0044f71eb31b5349fc74fab67087c70e5de2e
adoni5 <roryjmunro1@gmail.com>
2020-07-02 17:53:01 +0100

    Added mananger url to get HTML

--- reference/urls.py
+++ reference/urls.py
@@ -12,5 +12,10 @@ urlpatterns = [
         r'^api/v1/reference/upload/$',
         views.reference_list,
         name="reference_upload"
+    ),
+    url(
+        r'^api/v1/reference/manager/$',
+        views.reference_manager,
+        name="reference-manager"
     )
 ]
