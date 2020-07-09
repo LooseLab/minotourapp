8c01a2893d8e48bf7a24014e43a14351d673f4da
adoni5 <roryjmunro1@gmail.com>
2020-07-09 17:27:21 +0100

    Add config to check  media root file exist on reference start up

--- reference/apps.py
+++ reference/apps.py
@@ -1,5 +1,19 @@
+"""Apps config"""
 from django.apps import AppConfig
 
 
 class ReferenceConfig(AppConfig):
+    """
+    Config reference App
+    """
     name = 'reference'
+
+    def ready(self):
+        """
+        Hook that is called when the Reference app is initialised and ready. Code below is ready.
+        Returns
+        -------
+
+        """
+        from reference.utils import check_media_roots_exist
+        check_media_roots_exist()
