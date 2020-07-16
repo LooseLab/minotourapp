f1fc15ebe0838e6629fef94552022fbbbd12df7c
adoni5 <roryjmunro1@gmail.com>
2020-07-16 11:26:06 +0100

    Set null for deleted references

--- alignment/models.py
+++ alignment/models.py
@@ -34,21 +34,40 @@ class PafRoughCov(models.Model):
         blank=True,
     )
     read_type = models.ForeignKey(
-        FastqReadType, related_name="prc_type", on_delete=models.DO_NOTHING,
+        FastqReadType, related_name="prc_type", on_delete=models.SET_NULL, null=True
     )
     is_pass = models.BooleanField()  # pass = true, fail = false
     barcode = models.ForeignKey(
-        Barcode, related_name="prc_barcode", null=True, on_delete=models.DO_NOTHING,
+        Barcode, related_name="prc_barcode", null=True, on_delete=models.SET_NULL,
     )
     reference = models.ForeignKey(
-        ReferenceInfo, related_name="paf_reference", on_delete=models.DO_NOTHING,
+        ReferenceInfo, related_name="paf_reference", on_delete=models.SET_NULL, null=True
     )
     chromosome = models.ForeignKey(
-        ReferenceLine, related_name="paf_chromosome", on_delete=models.DO_NOTHING,
+        ReferenceLine, related_name="paf_chromosome", on_delete=models.SET_NULL, null=True
     )
     bin_position_start = models.IntegerField(default=0)  # position
     bin_position_end = models.IntegerField(default=0)
     bin_coverage = models.IntegerField(default=0)
+    # TODO I don't like this solution
+    # TODO we could(should?) have this as a third table
+    reference_pk = models.IntegerField(
+        default=0
+    )
+    reference_name = models.CharField(
+        max_length=256,
+        null=True
+    )
+    chromosome_length = models.IntegerField(
+        default=0
+    )
+    chromosome_pk = models.IntegerField(
+        default=0
+    )
+    chromosome_name = models.CharField(
+        max_length=256,
+        null=True
+    )
 
     def __str__(self):
         return f"{self.flowcell} {self.bin_position_start} - {self.bin_position_end}, Coverage: {self.bin_coverage}"
@@ -76,7 +95,7 @@ class PafSummaryCov(models.Model):
         null=True,
     )
     # TODO to be deleted, use field from chromosome
-    reference_line_length = models.BigIntegerField()
+    reference_line_length = models.IntegerField()
     read_count = models.BigIntegerField(default=0)
     total_yield = models.BigIntegerField(default=0)
     read_type = models.ForeignKey(
@@ -90,6 +109,20 @@ class PafSummaryCov(models.Model):
     )
     average_read_length = models.IntegerField(
         default=0
+    )
+    chromosome_pk = models.IntegerField(
+        default=0
+    )
+    chromosome_name = models.CharField(
+        max_length=256,
+        null=True
+    )
+    reference_pk = models.IntegerField(
+        default=0
+    )
+    reference_name = models.CharField(
+        max_length=256,
+        null=True
     )
 
     def __str__(self):
