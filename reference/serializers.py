887409c474f3bab9c47ad68fb85b0ad5a39c656c
adoni5 <roryjmunro1@gmail.com>
2020-07-16 11:59:22 +0100

    Add uploader Id to serialiser

--- reference/serializers.py
+++ reference/serializers.py
@@ -14,6 +14,7 @@ class ReferenceInfoSerializer(serializers.ModelSerializer):
             'name',
             'length',
             'private',
+            'uploader_id'
         )
         read_only = (
             'id',
