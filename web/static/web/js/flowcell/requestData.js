553721adc87b08a91f58b05161ba073bd6d01901
adoni5 <roryjmunro1@gmail.com>
2020-08-25 11:26:33 +0100

    Remove unused request data conditions, leveing just a last couple to be redone

--- web/static/web/js/flowcell/requestData.js
+++ web/static/web/js/flowcell/requestData.js
@@ -1,6 +1,5 @@
 function requestData (flowcell_id) {
   console.log(`Requesting data`)
-  var selected_barcode = getSelectedBarcode(`Metagenomics`)
   var flowcell_id = flowcell_id
 
   var url_run = `/api/v1/flowcells/` + flowcell_id + `/`
@@ -30,42 +29,6 @@ function requestData (flowcell_id) {
     } else if (flowcellSelectedTabInput == `live-event-data`) {
       // this.requestRunDetails(flowcellId);
       this.requestLiveRunStats(flowcell_id)
-    } else if (flowcellSelectedTabInput == `metagenomics`) {
-      // The intervals for updating the charts are found in the individual files in the vis-d3 directory
-      // SO you are on the Metagenomics tab, congratulations!
-      // const addBarcodes = this.addMetaBarcodeTabs.bind(this)
-      // // if (selected_barcode == null) {
-      // //
-      // // }
-      // const url = `/api/v1/flowcells/` + flowcell_id + `/metagenomic_barcodes`
-      //
-      // this.drawSankey(flowcell_id)
-      //
-      // this.metaHeader(flowcell_id)
-      // // Draw the donut rank table
-      // this.drawDonutRankTable(flowcell_id)
-      // // Draw the donut chart
-      // this.drawDonut(flowcell_id)
-      // // update the total Reads Table
-      // this._getTotalReadsTable(flowcell_id)
-      // // Draw the alert mapping targets table;
-      // this.update_mapping_table(flowcell_id)
-      // // draw the simplified results table
-      // this.draw_simple_table(flowcell_id)
-      //
-      // $.get(url, {}, function (result) {
-      //   this.barcodes = result.data.sort()
-      //
-      //   this.tabs = result.tabs
-      //   // draw the sankey
-      //   addBarcodes(flowcell_id, this.barcodes, this.tabs)
-      //   // update the metadata header
-      // })
-    } else if (flowcellSelectedTabInput == `advanced-sequence-mapping`) {
-      // this.requestAdvancedPafData(flowcellId);
-      this.drawReadUntilCharts()
-    } else if (flowcellSelectedTabInput == `sequence-assembly`) {
-      this.requestGfaData(flowcell_id)
     }
   }.bind(this))
 };
