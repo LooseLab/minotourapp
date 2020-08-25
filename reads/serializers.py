3ea8e77422f16c4d16fca851b59f369f5cd2bced
adoni5 <roryjmunro1@gmail.com>
2020-08-25 11:27:00 +0100

    Remove creation of mapping jobs on the creation of metagenomics tasks

--- reads/serializers.py
+++ reads/serializers.py
@@ -5,7 +5,7 @@ from django.conf import settings
 from rest_framework import serializers
 
 from communication.models import NotificationConditions
-from metagenomics.models import CentrifugeOutput, MappingTarget
+from metagenomics.models import CentrifugeOutput
 from reads.models import (Barcode, FastqFile, FastqRead,
                           FastqReadType, MinionControl,
                           MinionEvent, MinionEventType, MinionMessage,
@@ -13,7 +13,6 @@ from reads.models import (Barcode, FastqFile, FastqRead,
                           MinionInfo, Run, UserOptions, ChannelSummary, HistogramSummary,
                           RunStatisticBarcode, RunSummaryBarcode, GroupRun, FlowcellSummaryBarcode, Flowcell, Minion,
                           JobType, JobMaster)
-from reference.models import ReferenceInfo
 
 
 class FastqFileSerializer(serializers.HyperlinkedModelSerializer):
@@ -523,9 +522,9 @@ class GroupRunSerializer(serializers.HyperlinkedModelSerializer):
 
 class FlowcellSummaryBarcodeSerializer(serializers.ModelSerializer):
 
+
     class Meta:
         model = FlowcellSummaryBarcode
-
         fields = (
             'flowcell',
             'read_type_name',
@@ -550,7 +549,6 @@ class JobMasterSerializer(serializers.ModelSerializer):
     task_type_name = serializers.ReadOnlyField(
         source="job_type.name"
     )
-
     reference_name = serializers.ReadOnlyField(
         default="N/A",
         source="reference.name"
@@ -559,10 +557,9 @@ class JobMasterSerializer(serializers.ModelSerializer):
         source="job_type.private"
     )
 
+
     class Meta:
-
         model = JobMaster
-
         fields = (
             "id",
             "flowcell",
@@ -599,7 +596,6 @@ class JobMasterInsertSerializer(serializers.ModelSerializer):
         Check that reference exists if type is Minimap2
         """
         if data['job_type'].id in [4, 16] and data['reference'] is None:
-
             raise serializers.ValidationError("Reference is mandatory for this task.")
 
         # If the job type is minimap check we can find the minimap2 executable
@@ -607,44 +603,33 @@ class JobMasterInsertSerializer(serializers.ModelSerializer):
             if not Path.exists(Path(getattr(settings, "MINIMAP2", None))):
                 raise serializers.ValidationError("Minimap2 executable not found."
                                                   " Please configure in the environmental variables file.")
-
         # If it is not metagenomics double check the target set is None
         if data["job_type"].id != 10:
-
             data["target_set"] = None
-
         # Check if a metagenomics task is already running on this flowcell, if so refuse to create a new one
         elif JobMaster.objects.filter(
                 flowcell=data["flowcell"]).filter(job_type__name="Metagenomics").count() > 0:
-
             raise serializers.ValidationError("A metagenomics task is already running for this flowcell.")
-
         # If the job isn't EB or minimap2 or artic
         if data["job_type"].id not in [4, 15, 16]:
             data["reference"] = None
-
         if data["job_type"].id == 16:
             if JobMaster.objects.filter(
                 flowcell=data["flowcell"]).filter(job_type_id=16).count() > 0:
                 raise serializers.ValidationError("An artic task is already running for this flowcell.")
-
         # If the job is to calculate a sankey diagram
         if data["job_type"].id == 13:
             # Get the metagenomics task object
             metagenomics_task = JobMaster.objects.filter(
                 flowcell=data["flowcell"]).filter(job_type__name="Metagenomics")
-
             # If we don't have a metagenomics task we can't calculate a sankey diagram
             if metagenomics_task.count() != 1:
                 raise serializers.ValidationError("No metagenomics task found for this flowcell,"
                                                   " please start a metagenomics task before calculating"
                                                   " a lineages diagram")
-
             # If we don't have enough data to generate a sankey
             elif CentrifugeOutput.objects.filter(task=metagenomics_task[0]).count() < 10:
-
                 raise serializers.ValidationError("Not enough metagenomics data, please try again in a few minutes")
-
         return data
 
     def create(self, validated_data):
@@ -665,54 +650,4 @@ class JobMasterInsertSerializer(serializers.ModelSerializer):
             # save the changes
             flowcell_to_update_activity.save()
 
-            # If we have created a metagenomics task
-            if validated_data["job_type"].name == "Metagenomics":
-                # We have to create "Other" job types to do the validation mapping
-                # Get the jobtype for mapping target reads
-                job_type = JobType.objects.get(name="Other")
-
-                # get the target set we need
-                if "target_set" != None:
-                    mapping_target = list(MappingTarget.objects.filter(target_set=validated_data["target_set"])
-                                          .values_list("species", "name", "gff_line_type"))
-
-                else:
-                    mapping_target = None
-
-                if mapping_target:
-                    # Only create one gene entry per species, by counting how many are created in this dictionary,
-                    # doesn't really need to be a dict
-                    counts_dict = {}
-                    # Set to store unique species, target type ( gene or plasmid )
-                    setty = set()
-                    # Holding copy is a copy of the mapping targets, to append to below
-                    holding_copy = mapping_target
-                    # loop through references and create a job to map any that are only plasmid targets have
-                    for target in mapping_target:
-                        # Get unique types of gff regions for each species
-                        setty.add((target[0], target[2]))
-                        # If we don't have just for the reference but have one for a plasmid,
-                        # add a new gene entry to holding copy
-                        if (target[0], "gene") not in setty and (target[0], "", "gene") not in mapping_target:
-                            holding_copy.append((target[0], "", "gene"))
-                    # Loop through and create the mapping job masters
-                    for hold in holding_copy:
-                        # Check to see whether we have a gene entry for this species in the dictionary, if we do skip
-                        if (hold[0], hold[2]) not in counts_dict and hold[2] == "gene":
-                            counts_dict[(hold[0], hold[2])] = 1
-                        elif (hold[0], hold[2]) in counts_dict and hold[2] == "gene":
-                            continue
-                        # get the correct reference for that JobMaster
-                        if hold[2] == "plasmid":
-                            # TODO temporary fix
-                            refer = ReferenceInfo.objects.filter(name=hold[0].replace(" ", "_") + "_" + hold[1]).first()
-                        else:
-                            # TODO temporary fix
-                            refer = ReferenceInfo.objects.filter(name=hold[0].replace(" ", "_")).first()
-
-                        jm, created = JobMaster.objects.get_or_create(job_type=job_type, reference=refer,
-                                                             flowcell=validated_data["flowcell"],
-                                                             complete=True)
-                        jm.save()
-            # ############################ End of other mapping creation
         return job_master
