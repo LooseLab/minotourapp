9d3a8c34ede7e1ffb6f5b158313957146f57f611
adoni5 <roryjmunro1@gmail.com>
2020-08-27 15:15:14 +0100

    Updated during clean install script now works

--- minotourapp/settings.py
+++ minotourapp/settings.py
@@ -41,7 +41,6 @@ INSTALLED_APPS = [
     'django.contrib.humanize',
     'rest_framework',
     'rest_framework.authtoken',
-    'django_celery_beat',
     'django_extensions',
     'reads',
     'web',
