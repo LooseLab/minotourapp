a62bc99ad60bd9fbe22e72dfe7b64b665a47e90d
adoni5 <roryjmunro1@gmail.com>
2020-08-27 14:14:31 +0100

    Fix documentation, example env variable file, tmux script and upgrade minimap2 version

---
+++ envs.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+export MT_DB_ENGINE='django.db.backends.mysql'
+export MT_DB_HOST='localhost'
+#export MT_DB_NAME='django_test'
+export MT_DB_NAME='<mysql database name>'
+export MT_DB_USER='<mysql database user>>'
+export MT_DB_PASS='<mysql database password>'
+export MT_DB_PORT='3306'
+export MT_DJANGO_DEBUG=false
+export MT_MAILGUN_ACCESS_KEY=''
+export MT_MAILGUN_SERVER_NAME=''
+export MT_MINIMAP2='extra/minimap2-2.17_x64-linux/minimap2'
+export MT_REFERENCE_LOCATION='<path/to/references/directory/>' # Write uploaded references to this directory
+export MT_TWITCONSUMER_KEY='<Twitter consumer key>'
+export MT_TWITCONSUMER_SECRET='<Twitter consumer secret>'
+export MT_TWITTOKEN='<Twitter token>'
+export MT_TWITTOKEN_SECRET='<Twitter token secret>'
+# Generate a secret key with python manage.py shell -c 'from django.core.management import utils; print(utils.get_random_secret_key())'
+export MT_SECRET_KEY='<Add secret key here>'
+export MT_CENTRIFUGE="extra/centrifuge-1.0.4-beta/centrifuge"
+export MT_CENTRIFUGE_INDEX="<Path/to/centrifuge/index/index_name>" # Read centrifuge index from this directory
+export MT_LOG_FOLDER='<Path/To/Log/Directory>' # Write Celery log file in this directory
+export MT_CELERY_BROKER_URL='redis://localhost:6379/0'
+export MT_CELERY_RESULT_BACKEND='redis://localhost:6379/0'
+#export MT_ARTIC_RESULTS_DIR='/media/rory/Bioinformatics'
+export MT_ARTIC_RESULTS_DIR='<Path/To/Artic/Results/Directory>' # Store the Artic results in this directory
+export MT_ARTIC_SCEHEME_DIR='<Path/To/Artic/Code/Scheme_Directories>' # Read the Artic scheme directories, found where the Artic NCOV piprline github repo was clones. Example  Projects/artic-ncov2019/primer_schemes
+export MT_ARTIC_MAX_PIPELINES="1" # Maximum number of Artic tasks to be run at any one time
+export MT_CELERY_WORKER_COUNT="2" # Number of Celery workers to start, no more than number of CPU cores
+export MT_CELERY_MINIMAP2_WORKER_COUNT="1" # Number of celery minimap2 threads to start, no more than num CPU cores
+export MT_TIME_UNTIL_INACTIVE="7" # Number of days since last flowcell activity until a flowcell is listed as inactive
+export MT_TIME_UNTIL_ARCHIVE="14" # Number of days since last flowcell activity until a flowcell is archived and fastq data deleted
---
+++ scripts/run_minotour_tmux.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+# configure your environment variable file found at minotourapp/envs.sh
+source envs.sh
+NAME=$1
+WORKER_COUNT=$MT_CELERY_WORKER_COUNT
+
+# If name was not specified use $USER                                           
+[ -z "$NAME" ] && NAME=$USER
+
+# If tmux has a session with name, attach that and exit                         
+tmux has-session -t $NAME 2> /dev/null                                          
+if [ $? -eq 0 ]; then                                                            
+    tmux attach -t $NAME                                                        
+    exit 0;                                                                     
+fi
+
+tmux new-session -d -s $NAME \;
+tmux new-window -a -n dshell \;
+tmux select-window -t dshell \;
+tmux send-keys "cd .." C-m \;
+# If the python environment doesn't exist
+if [ ! -f ../extra/minotour/bin/activate ]; then
+    tmux send-keys "python -m venv extra/minotour" C-m \;
+    tmux send-keys "source extra/minotour/bin/activate" C-m \;
+    tmux send-keys "pip -r install requirements.txt" C-m \;
+fi
+tmux send-keys "source ../extra/minotour/bin/activate" C-m \;
+tmux send-keys ". envs.sh" C-m \;
+tmux send-keys "python manage.py shell_plus" C-m \; 
+tmux new-window -a -n minotour \; 
+tmux select-window -t minotour \; 
+tmux send-keys "cd .." C-m \;
+tmux send-keys "source ../extra/minotour/bin/activate" C-m \;
+tmux send-keys ". envs.sh" C-m \;
+tmux send-keys "python manage.py runserver 8100" C-m \;
+tmux split-window -v -p 70 \; 
+tmux send-keys "cd .." C-m \;
+tmux send-keys "source ../extra/minotour/bin/activate" C-m \;
+tmux send-keys ". envs.sh" C-m \;
+tmux send-keys "celery -A minotourapp worker -l info -f ~/data/logs/celery.log --concurrency $WORKER_COUNT -Ofair" C-m \;
+tmux split-window -h \; 
+tmux send-keys "mtlog" C-m \;
+tmux new-window -a -n zshelly \;
+tmux select-window -t zshelly\;
+tmux send-keys "cd .." C-m \;
+tmux send-keys "source ../extra/minotour/bin/activate" C-m \;
+tmux send-keys ". envs.sh" C-m \;
+tmux new-window -a -n minimap2Celery \;
+tmux select-window -t minimap2Celery \;
+tmux send-keys "cd .." C-m \;
+tmux send-keys "source ../extra/minotour/bin/activate" C-m \;
+tmux send-keys ". envs.sh" C-m \;
+tmux send-keys "celery -A minotourapp worker -l info -f $MT_LOG_FOLDER/celery.log -n minimap2 --concurrency $MT_CELERY_MINIMAP2_WORKER_COUNT -Ofair -Q minimap --pool threads" C-m \;
+tmux split-window -h \;
+tmux send-keys "cd .." C-m \;
+tmux send-keys "source ../extra/minotour/bin/activate" C-m \;
+tmux send-keys ". envs.sh" C-m \;
+tmux send-keys "celery beat -A minotourapp -l info" C-m \;
+tmux split-window -v \;
+tmux send-keys "mtlog | grep align" C-m \;
+tmux new-window -a -n mysql \;
+tmux select-window -t mysql \;
+tmux send-keys "mysql -u root -p issue_192" C-m \;
+tmux new-window -a -n flower \;
+tmux select-window -t flower \;
+tmux send-keys "cd .." C-m \;
+tmux send-keys "source ../extra/minotour/bin/activate" C-m \;
+tmux send-keys ". envs.sh" C-m \;
+tmux send-keys "flower -A minotourapp --port=5556" C-m \;
+
+tmux attach -t $NAME
