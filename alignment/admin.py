d9683b7e43b08ff635c51c358af55cae00427440
adoni5 <roryjmunro1@gmail.com>
2020-09-10 09:44:41 +0100

    Add PafRoughCov Intermediate into admin interface

--- alignment/admin.py
+++ alignment/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import PafRoughCov, PafSummaryCov
+from .models import PafRoughCov, PafSummaryCov, PafRoughCovIntermediate
 
 admin.site.register(PafRoughCov)
 admin.site.register(PafSummaryCov)
+admin.site.register(PafRoughCovIntermediate)
