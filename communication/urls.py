18288ac7a0ca0fda8831f075cd5dbc3fb72e6af9
adoni5 <roryjmunro1@gmail.com>
2020-09-14 15:32:07 +0100

    Add sent messages api endpoint to communication app

--- communication/urls.py
+++ communication/urls.py
@@ -4,25 +4,33 @@ from communication import views
 
 urlpatterns = [
     url(
-        r'^api/v1/communication/conditions/create-condition/$',
+        r"^api/v1/communication/conditions/create-condition/$",
         views.simple_condition_create,
-        name="simple-create"
-    ), url(
-        r'^api/v1/communication/(?P<pk>[0-9]+)/conditions/references$',
-        views.get_references_for_condtions,
-        name="get-references"
+        name="simple-create",
     ),
     url(
-        r'^api/v1/communication/messages/conditions$',
+        r"^api/v1/communication/(?P<pk>[0-9]+)/conditions/references$",
+        views.get_references_for_condtions,
+        name="get-references",
+    ),
+    url(
+        r"^api/v1/communication/messages/conditions$",
         views.notification_conditions_list,
-        name="conditions"
-    ), url(
-        r'^api/v1/communication/(?P<pk>[0-9]+)/conditions/chromosomecoverage/$',
+        name="conditions",
+    ),
+    url(
+        r"^api/v1/communication/(?P<pk>[0-9]+)/conditions/chromosomecoverage/$",
         views.get_coverage_summary,
-        name="get_coverage_summary"
-    ), url(
-        r'^api/v1/communication/(?P<pk>[0-9]+)/conditions/extant-types/$',
+        name="get_coverage_summary",
+    ),
+    url(
+        r"^api/v1/communication/(?P<pk>[0-9]+)/conditions/extant-types/$",
         views.get_extant_condition_types,
-        name="get_extant_conditions"
-    )
+        name="get_extant_conditions",
+    ),
+    url(
+        r"^api/v1/communication/sent-tweets/$",
+        views.new_messages_list,
+        name="sent-messages-list",
+    ),
 ]
