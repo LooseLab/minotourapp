9b9adf168412f931e2ba4722cb13ca7305e5f181
adoni5 <roryjmunro1@gmail.com>
2020-09-14 15:31:21 +0100

    Eslint file and remove mad new lines

--- web/static/web/js/flowcell/requestReadData.js
+++ web/static/web/js/flowcell/requestReadData.js
@@ -1,53 +1,47 @@
-function requestReadData(id) {
+function requestReadData (id) {
+  var table
 
-    var table;
+  if ($.fn.dataTable.isDataTable(`#example`)) {
+    table = $(`#example`).DataTable()
+  } else {
+    table = $(`#example`).DataTable({
+      processing: true,
+      serverSide: true,
+      pagingType: `simple`,
+      deferLoading: 0,
+      ajax: {
+        url: `/web/private/flowcells_reads_data/`,
+        data: {
+          flowcell_id: id
+        }
+      },
+      columns: [
+        { data: `read_id` },
+        { data: `read` },
+        { data: `channel` },
+        { data: `sequence_length` },
+        { data: `run__runid` },
+        { data: `barcode__name` },
+        {
+          className: `details-control`,
+          sortable: false,
+          data: null,
+          defaultContent: `<button class="btn btn-primary" type="button">Show sequence</button>`
+        }
+      ]
+    })
 
-    if ( $.fn.dataTable.isDataTable( '#example' ) ) {
+    $(`#example`).on(`click`, `td.details-control`, function () {
+      var tr = $(this).closest(`tr`)
+      var row = table.row(tr)
 
-        table = $('#example').DataTable();;
-
-    } else {
-
-        table = $('#example').DataTable({
-            "processing": true,
-            "serverSide": true,
-            "pagingType": 'simple',
-            "deferLoading": 0,
-            "ajax": {
-                "url": '/web/private/flowcells_reads_data/',
-                "data": {
-                    "flowcell_id": id
-                },
-            },
-            "columns": [
-                {"data": "read_id"},
-                {"data": "read"},
-                {"data": "channel"},
-                {"data": "sequence_length"},
-                {"data": "run__runid"},
-                {"data": "barcode__name"},
-                {
-                    'className': 'details-control',
-                    'sortable': false,
-                    'data': null,
-                    'defaultContent': "<button class=\"btn btn-primary\" type=\"button\">Show sequence</button>"
-                },
-            ]
-        });
-
-        $('#example').on('click', 'td.details-control', function () {
-
-            var tr = $(this).closest('tr');
-            var row = table.row(tr);
-
-            if (row.child.isShown()) {
-                row.child.hide();
-                tr.removeClass('shown');
-            }
-            else {
-                row.child(format(row.data())).show();
-                tr.addClass('shown');
-            }
-        });
-    }
+      if (row.child.isShown()) {
+        row.child.hide()
+        tr.removeClass(`shown`)
+      } else {
+        row.child(format(row.data())).show()
+        tr.addClass(`shown`)
+      }
+    })
+  }
 }
