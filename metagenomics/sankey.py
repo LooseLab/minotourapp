cec3a7372e0032d0fee252dfa28995af71be4d0d
adoni5 <roryjmunro1@gmail.com>
2020-09-22 16:02:25 +0100

    Remove blank new lines

--- metagenomics/sankey.py
+++ metagenomics/sankey.py
@@ -41,26 +41,19 @@ def calculate_sankey_values(lineages_df, flowcell, tax_rank_filter, metagenomics
     # ##############  Storing snakey links and nodes ################
 
     logger.info("Flowcell id: {} - Starting Sankey Calculations".format(flowcell.id))
-
     # set index to tax_id
     lineages_df.set_index("tax_id", inplace=True)
-
     # The columns we actually need
     columns = ["num_matches", "sum_unique", "barcode_name", "superkingdom", "phylum", "class", "order", "family",
                "genus", "species"]
-
     # Subset the dataframe into those columns
     sankey_lineages_df = lineages_df[columns]
-
     # Backfill the dataframe nas with the lowest common entry, if family is missing, and genus
     #  isn't, family NaN becomes the genus entry
     sankey_lineages_df = sankey_lineages_df.fillna(axis=1, method="bfill")
-
     sankey_lineages_df = sankey_lineages_df.sort_values("num_matches", ascending=False)
-
     # Rank lineages to flow through
     sankey_lineages_df["path"] = np.arange(0, sankey_lineages_df.shape[0], 1)
-
     # initialise the series that will become our sankey pandas DataFrame
     source = pd.Series()
     target = pd.Series()
@@ -95,48 +88,34 @@ def calculate_sankey_values(lineages_df, flowcell, tax_rank_filter, metagenomics
             sankey_lineages_df["target_tax_level"] = target_tax_rank
             target_tax_level = target_tax_level.append(sankey_lineages_df["target_tax_level"])
             path = path.append(sankey_lineages_df["path"])
-
     # Create a DataFrame of links
     source_target_df = pd.concat([source, target, value, tax_id, target_tax_level, barcode, path], axis=1)
-
     # Rename the columns
     source_target_df.columns = ["source", "target", "value", "tax_id", "target_tax_level", "barcode_name", "path"]
-
     logger.info(source_target_df)
-
     # Create a series of Job_master objects through broadcasting
     source_target_df["task"] = metagenomics_task
     # Broadcast the the job_master object down the dataframe for this job
     source_target_df["sankey_task"] = sankey_task
-
     logger.info("Flowcell id: {} - Barcodes are:".format(flowcell.id))
     logger.info(source_target_df["barcode_name"].unique())
-
-    # TODO DO sorting magic before the database deposition
     # Create a dataframe of the previous results for this metagenomics task
-
     previous = SankeyLink.objects.filter(task=metagenomics_task, barcode_name=barcode_name)
     if previous:
         previous.delete()
     # If there no results, i.e this is the first iteration
-
     to_create_sankey_df = source_target_df
     # if there are results these need updating where we have the same link
-
     # ## ### Create the SankeyLinks object #### # One for each link ## ### #
     to_create_sankey_df.reset_index(inplace=True)
-
     # Drop duplicates as we only need to create one
     to_create_sankey_df = to_create_sankey_df.drop_duplicates(subset=["tax_id", "target_tax_level"])
-
     # If there are values that need creating
     if not to_create_sankey_df.empty:
         # Perform the apply to create the series to create
         sankey_models_for_bulk_insert = to_create_sankey_df.apply(create_sankeylink_models, axis=1)
-
         # Bulk create the objects
         SankeyLink.objects.bulk_create(list(sankey_models_for_bulk_insert.values))
-
     logger.info("Flowcell id: {} - Finished updating sankey links".format(flowcell.id))
 
 
@@ -147,32 +126,18 @@ def calculate_sankey(flowcell_job_id):
     :return:
     """
     task = JobMaster.objects.get(pk=flowcell_job_id)
-
     flowcell = task.flowcell
-
     task.running = True
-
     task.save()
-
     metagenomics_task = JobMaster.objects.get(flowcell=flowcell, job_type__name="Metagenomics")
-
     tax_rank_filter = ["superkingdom", "phylum", "class", "order", "family", "genus", "species"]
-
     barcodes = CentrifugeOutput.objects.filter(task=metagenomics_task).values_list("barcode_name", flat=True).distinct()
-
     for barcode in barcodes:
-
         df = pd.DataFrame(list(CentrifugeOutput.objects.filter(
             task=metagenomics_task, barcode_name=barcode).order_by("-num_matches").values()[:30]))
-
         df.rename(columns={"classy": "class"}, inplace=True)
-
         logger.info(df.head())
-
         calculate_sankey_values(df, flowcell, tax_rank_filter, metagenomics_task, task, barcode)
-
     task.running = False
-
     task.complete = True
-
     task.save()
