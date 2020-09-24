a25588a2bc78c5f5e03f9698f7239df0059dd7d4
adoni5 <roryjmunro1@gmail.com>
2020-09-24 10:49:21 +0100

    Es line file to conform with standards

--- web/static/web/js/services/FlowcellService.js
+++ web/static/web/js/services/FlowcellService.js
@@ -1,20 +1,17 @@
 class FlowcellService {
+  constructor () {
+    this._http = new HttpService()
+  }
 
-    constructor() {
-        this._http = new HttpService();
-    }
-
-    getFlowcellTabs(flowcell_id) {
-
-        return this._http
-            .get(`/api/v1/flowcells/${flowcell_id}/tabs/`)
-            .then(tabs => {
-
-                return tabs;
-            })
-            .catch(erro => {
-                console.log(erro);
-                throw new Error('Error getting the flowcell tabs.');
-            });
-    }
+  getFlowcellTabs (flowcell_id) {
+    return this._http
+      .get(`/api/v1/flowcells/${flowcell_id}/tabs/`)
+      .then(tabs => {
+        return tabs
+      })
+      .catch(erro => {
+        console.log(erro)
+        throw new Error(`Error getting the flowcell tabs.`)
+      })
+  }
 }
