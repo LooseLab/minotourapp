5ae19fed101dab23a96411a861a086dd878fc62c
adoni5 <roryjmunro1@gmail.com>
2021-01-18 12:12:00 +0000

    Alter the artic fire conditions to be amplicon based

--- scripts/change_artic_fire_conditions.sql
+++ scripts/change_artic_fire_conditions.sql
@@ -15,3 +15,21 @@ ALTER TABLE `artic_articfireconditions` DROP COLUMN `ninety_percent_bases_at`;
 --
 ALTER TABLE `artic_articfireconditions` ADD COLUMN `percent_of_amplicons` integer DEFAULT 90 NOT NULL;
 ALTER TABLE `artic_articfireconditions` ALTER COLUMN `percent_of_amplicons` DROP DEFAULT;
+--
+-- Remove field percentage_bases_at_90_value from articbarcodemetadata
+--
+ALTER TABLE `artic_articbarcodemetadata` DROP COLUMN `percentage_bases_at_90_value`;
+--
+-- Remove field percentage_bases_at_95_value from articbarcodemetadata
+--
+ALTER TABLE `artic_articbarcodemetadata` DROP COLUMN `percentage_bases_at_95_value`;
+--
+-- Remove field percentage_bases_at_99_value from articbarcodemetadata
+--
+ALTER TABLE `artic_articbarcodemetadata` DROP COLUMN `percentage_bases_at_99_value`;
+--
+-- Add field percent_amp_at_x_cov to articfireconditions
+--
+ALTER TABLE `artic_articfireconditions` ADD COLUMN `percent_amp_at_x_cov` double precision DEFAULT 0.0e0 NOT NULL;
+ALTER TABLE `artic_articfireconditions` ALTER COLUMN `percent_amp_at_x_cov` DROP DEFAULT;
+
