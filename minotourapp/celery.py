ff039c34e3de2282a24ced4e035a6ff961aacd6c
adoni5 <roryjmunro1@gmail.com>
2021-01-18 12:11:40 +0000

    Auto write out to a log file in the minotourapp directory errored celery tasks and timed out celery tasks

--- minotourapp/celery.py
+++ minotourapp/celery.py
@@ -1,18 +1,57 @@
 from __future__ import absolute_import, unicode_literals
 
+import datetime
+import logging
 import os
 
-from celery import Celery
+from celery import Celery, Task
+from celery.worker.request import Request
+
+from minotourapp.settings import BASE_DIR
+
+logger = logging.getLogger("my.package")
+
+
+class MyRequest(Request):
+    """A minimal custom request to log failures and hard time limits."""
+
+    def on_timeout(self, soft, timeout):
+        super(MyRequest, self).on_timeout(soft, timeout)
+        error_log_file = f"{BASE_DIR}/celery_error.log"
+        print(dir(self))
+        with open(error_log_file, "a") as fh:
+            fh.write(
+                f"{self.task.name} timed out at {datetime.datetime.now()}"
+                f" processing {self.task.args} \n {self.task.kwargs}"
+            )
+
+    def on_failure(self, exc_info, send_failed_event=True, return_ok=False):
+        super(Request, self).on_failure(
+            exc_info, send_failed_event=send_failed_event, return_ok=return_ok
+        )
+        error_log_file = f"{BASE_DIR}/celery_error.log"
+        print(dir(self))
+        with open(error_log_file, "a") as fh:
+            fh.write(
+                f"{self.task.name} failed at {datetime.datetime.now()}"
+                f" processing {self.task.args} \n {self.task.kwargs}"
+            )
+
+
+class MyTask(Task):
+    Request = MyRequest
+
 
 # set the default Django settings module for the 'celery' program.
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'minotourapp.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "minotourapp.settings")
 
-app = Celery('minotourapp')
+app = Celery("minotourapp")
+
 
 # Using a string here means the worker don't have to serialize
 # the configuration object to child processes.
 # - namespace='CELERY' means all celery-related configuration keys
 #   should have a `CELERY_` prefix.
-app.config_from_object('django.conf:settings', namespace='CELERY')
+app.config_from_object("django.conf:settings", namespace="CELERY")
 
 app.autodiscover_tasks()
