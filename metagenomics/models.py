a20160d4d04781ae31375eab5c14739fa36f3e7d
adoni5 <roryjmunro1@gmail.com>
2021-03-22 18:10:30 +0000

    Add estimated abundance model for Targeted metagenomics

--- metagenomics/models.py
+++ metagenomics/models.py
@@ -10,6 +10,29 @@ from reads.models import Barcode, JobMaster
 from reference.models import ReferenceInfo
 
 
+class EstimatedAbundance(models.Model):
+    """
+    :purpose: Store estimated abundances of targets in a metagenomics run
+    :author: Rory Munro
+    """
+    task = models.ForeignKey(
+        JobMaster,
+        related_name="estimated_abundances",
+        on_delete=models.CASCADE
+    )
+    tax_id = models.IntegerField(
+        default=0
+    )
+    abundance = models.FloatField(
+        default=0.0
+    )
+    name = models.CharField(
+        default="",
+        null=True,
+        max_length=256
+    )
+
+
 class CAllValues(models.Model):
     """
     :purpose: Store the C_all values for abundance estimation across iterations for a metagenomics task
@@ -45,25 +68,17 @@ class SankeyLink(models.Model):
         to ProteoBacteria (Phylum)
     """
     source = models.CharField(null=True, max_length=100)
-
     target = models.CharField(null=True, max_length=100)
-
     value = models.IntegerField(null=True, default=0)
-
     barcode_name = models.CharField(null=True, max_length=100)
-
     tax_id = models.IntegerField()
-
     task = models.ForeignKey(
         JobMaster,
         related_name="sankey_link",
         on_delete=models.CASCADE
     )
-
     target_tax_level = models.CharField(max_length=100)
-
     path = models.IntegerField(default=0)
-
     def __str__(self):
         return "{} {} {} {}".format(self.barcode_name, self.tax_id, self.target_tax_level, self.task)
 
@@ -85,19 +100,14 @@ class Metadata(models.Model):
         :finish_time: (str) - The time the analysis finished
 
     """
-
     task = models.ForeignKey(
         JobMaster,
         related_name="metadata",
         on_delete=models.CASCADE
     )
-
     start_time = models.DateTimeField(default=timezone.now, null=True)
-
     finish_time = models.CharField(max_length=40, null=True)
-
     classified = models.IntegerField(default=0)
-
     unclassified = models.IntegerField(default=0)
 
 
@@ -119,39 +129,24 @@ class CentrifugeOutput(models.Model):
         indexes = [
             models.Index(fields=['tax_id'], name='tax_id_index'),
         ]
-
     name = models.CharField(max_length=250, null=True)
-
     tax_id = models.IntegerField(null=True)
-
     task = models.ForeignKey(
         JobMaster,
         related_name="centrifuge_output",
         on_delete=models.CASCADE,
     )
-
     num_matches = models.IntegerField(default=0)
-
     sum_unique = models.IntegerField(default=0)
-
     barcode_name = models.CharField(max_length=50)
-
     proportion_of_classified = models.FloatField(default=0.0)
-
     superkingdom = models.CharField(null=True, max_length=70)
-
     phylum = models.CharField(null=True, max_length=70)
-
     classy = models.CharField(null=True, max_length=70)
-
     order = models.CharField(null=True, max_length=70)
-
     family = models.CharField(null=True, max_length=70)
-
     genus = models.CharField(null=True, max_length=70)
-
     species = models.CharField(null=True, max_length=70)
-
     latest = models.IntegerField(null=True)
 
     def __str__(self):
@@ -168,27 +163,16 @@ class MappingResult(models.Model):
         on_delete=models.CASCADE,
         null=True
     )
-
-    species = models.CharField(max_length=50)
-
+    species = models.CharField(max_length=256)
     tax_id = models.IntegerField(null=True)
-
     barcode_name = models.CharField(max_length=50, default="All reads")
-
     num_mapped = models.IntegerField(default=0)
-
     mapped_proportion_of_classified = models.FloatField(default=0)
-
     red_reads = models.IntegerField(null=True, default=0)
-
     red_reads_proportion_of_classified = models.FloatField(default=0)
-
     num_matches = models.IntegerField(default=0)
-
     proportion_of_classified = models.FloatField(default=0.0)
-
     sum_unique = models.IntegerField(default=0)
-
     def __str__(self):
         return "{} {} {}".format(self.species, self.tax_id, self.task)
 
@@ -198,13 +182,11 @@ class TargetMappedReadId(models.Model):
         The read ids for reads that have identified dangerously
     """
     read_id = models.CharField(max_length=100, unique=True)
-
     mapping_result = models.ForeignKey(
         MappingResult,
         related_name="target_read_id",
         on_delete=models.CASCADE
     )
-
     def __str__(self):
         return self.read_id
 
@@ -213,36 +195,26 @@ class MappingTarget(models.Model):
     """
         The species defaults for the detection
     """
-    species = models.CharField(max_length=100)
-
+    species = models.CharField(max_length=100, null=True)
     tax_id = models.IntegerField(null=True)
-
     target_set = models.CharField(default="Default", max_length=100)
-
     start = models.IntegerField(default=0)
-
     end = models.IntegerField(default=1)
-
     name = models.CharField(default="danger_zone", max_length=50)
-
     gff_line_type = models.CharField(default="gene", max_length=100)
-
     owner = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         related_name='mapping_targets',
         on_delete=models.CASCADE,
         null=True
     )
-
     reference = models.ForeignKey(
         ReferenceInfo,
         related_name="mapping_target_reference",
         on_delete=models.CASCADE,
         null=True
     )
-
     private = models.BooleanField(default=False)
-
     def __str__(self):
         return "{} {} {}-{} {}".format(self.species, self.target_set, self.start, self.end, self.gff_line_type)
 
