5d89484870aa4d4c2c5c1a64b9b2528a5c5f0ac6
adoni5 <roryjmunro1@gmail.com>
2021-05-17 14:23:14 +0100

    New check media root moved in here

--- reference/apps.py
+++ reference/apps.py
@@ -1,8 +1,24 @@
 """Apps config"""
+from pathlib import Path
+
 from django.apps import AppConfig
+from django.conf import settings
 
-from reference.utils import check_media_roots_exist
 
+def check_media_roots_exist():
+    """
+    Check the django specified media root exists, and if not creates them
+    Returns
+    -------
+
+    """
+    media_root = Path(settings.MEDIA_ROOT)
+    if not media_root.exists():
+        media_root.mkdir()
+    if not (media_root / "minimap2_indexes").exists():
+        (media_root / "minimap2_indexes").mkdir()
+    if not (media_root / "reference_files").exists():
+        (media_root / "reference_files").mkdir()
 
 class ReferenceConfig(AppConfig):
     """
