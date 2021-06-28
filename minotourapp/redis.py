4d038e2e57bfdedeb7c22eeaee8fd0b996c6fd13
adoni5 <roryjmunro1@gmail.com>
2021-06-28 12:57:14 +0100

    fix to redis instance creation

--- minotourapp/redis.py
+++ minotourapp/redis.py
@@ -8,6 +8,6 @@ from minotourapp.utils import get_env_variable
 redis_instance = redis.StrictRedis(
     host="127.0.0.1", port=6379, db=1, decode_responses=True
 ) if "localhost" in get_env_variable("MT_DJANGO_REDIS_URL") else redis.StrictRedis(
-        unix_socket_path=get_env_variable("MT_DJANGO_REDIS_URL"), decode_responses=True
+        unix_socket_path=get_env_variable("MT_DJANGO_REDIS_URL").split("//")[-1], decode_responses=True
 )
 
