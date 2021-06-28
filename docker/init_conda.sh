c9169f2792033279fc1dd9f8dfc20f02e101a3c4
adoni5 <roryjmunro1@gmail.com>
2021-06-28 15:32:13 +0100

    fix conda init script

--- docker/init_conda.sh
+++ docker/init_conda.sh
@@ -1,7 +1,9 @@
+cd /var/lib/minotour/apps/fieldbioinformatics/
 conda activate artic
 python setup.py install
-artic -v
+artic -v;
+cd /var/lib/minotour/apps/pangolin/
 conda activate pangolin
 pip install .
 pangolin --version
-conda deactivate
+conda deactivate;
