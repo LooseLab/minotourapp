17db4c4b490b96fd6b2f08dab6802776c13ea67d
adoni5 <roryjmunro1@gmail.com>
2021-06-28 14:41:40 +0100

    Fix to set harvesting to 0 on launch

--- reads/apps.py
+++ reads/apps.py
@@ -4,5 +4,7 @@ from django.apps import AppConfig
 class ReadsConfig(AppConfig):
     name = 'reads'
     def ready(self):
-        import reads.services
+        # if docker instances shut with harvesting 1 we need to set it to 0
+        from minotourapp.redis import redis_instance
+        redis_instance.set("harvesting", 0)
 
