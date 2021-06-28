7eec2bc0f98abc357d97b7c50fa1ea1fc74b383b
adoni5 <roryjmunro1@gmail.com>
2021-06-28 15:04:56 +0100

    Try except the redis setting harvesting for building the image

--- reads/apps.py
+++ reads/apps.py
@@ -6,5 +6,9 @@ class ReadsConfig(AppConfig):
     def ready(self):
         # if docker instances shut with harvesting 1 we need to set it to 0
         from minotourapp.redis import redis_instance
-        redis_instance.set("harvesting", 0)
+        import redis
+        try:
+            redis_instance.set("harvesting", 0)
+        except redis.ConnectionError as e:
+            print(f"Are you building the container? {repr(e)}")
 
