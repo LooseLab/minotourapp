607149ab48e9dce36452e39251bb5ea6d8cc7a2c
adoni5 <roryjmunro1@gmail.com>
2021-08-03 16:59:13 +0100

    Added in the updated off-line copies of the javascript libraries into a new highcharts folder in the libraries folder, set the default_auto_field in settings.py and black formatted the git pull

--- artic/apps.py
+++ artic/apps.py
@@ -9,7 +9,8 @@ class ArticConfig(AppConfig):
     """
     The config file for the Artic app.
     """
-    name = 'artic'
+
+    name = "artic"
 
     def ready(self):
         """
@@ -19,26 +20,33 @@ class ArticConfig(AppConfig):
 
         """
         from artic.utils import check_artic_static_exists
+
         check_artic_static_exists()
         from minotourapp.utils import get_env_variable
+
         MT_VoC_PATH = get_env_variable("MT_VoC_PATH")
         if Path(f"{MT_VoC_PATH}").exists():
-            print ("VoC Path Found")
+            print("VoC Path Found")
             ##Check if
-            #cloned_repo = Repo.clone(os.path.join("https://github.com/phe-genomics/variant_definitions", Path(f"{MT_VoC_PATH}")))
+            # cloned_repo = Repo.clone(os.path.join("https://github.com/phe-genomics/variant_definitions", Path(f"{MT_VoC_PATH}")))
             if Path(f"{MT_VoC_PATH}/variant_definitions/").exists():
-                #already cloned so....
-                print ("Updating path")
+                # already cloned so....
+                print("Updating path")
                 try:
                     repo = Repo(Path(f"{MT_VoC_PATH}/variant_definitions/"))
                     print(repo.remotes.origin.pull())
                     pass
                 except git.GitCommandError as e:
-                    print(f"Git error, presumably being updated simultaneously {repr(e)}")
+                    print(
+                        f"Git error, presumably being updated simultaneously {repr(e)}"
+                    )
             else:
                 try:
-                    cloned_repo = Repo.clone_from("https://github.com/phe-genomics/variant_definitions", f"{MT_VoC_PATH}/variant_definitions/")
+                    cloned_repo = Repo.clone_from(
+                        "https://github.com/phe-genomics/variant_definitions",
+                        f"{MT_VoC_PATH}/variant_definitions/",
+                    )
                 except git.GitCommandError as e:
-                    print(f"Git error, presumably being updated simultaneously {repr(e)}")
-
-
+                    print(
+                        f"Git error, presumably being updated simultaneously {repr(e)}"
+                    )
