26c3a288c8594d339e14c2c85ff7f6c8a99ce25e
adoni5 <roryjmunro1@gmail.com>
2021-08-05 19:12:07 +0100

    Remove primer scheme model to reads models

--- artic/models.py
+++ artic/models.py
@@ -1,4 +1,5 @@
 """Model classes for the artic app."""
+from django.contrib.auth.models import User
 from django.db import models
 
 from minknow_data.models import Flowcell
@@ -56,23 +57,4 @@ class ArticBarcodeMetadata(models.Model):
 
     def __str__(self):
         return f"{self.flowcell} {self.job_master} {self.barcode}"
-
-
-class PrimerScheme(models.Model):
-    """
-    Store primer schemes uploaded to minoTour, stores scheme species and version
-    """
-    scheme_species = models.CharField(max_length=32)
-    scheme_versions = models.CharField(max_length=32)
-    bed_file = models.FileField(
-        upload_to="primer_schemes",
-        default="",
-        null=True,
-        max_length=256
-    )
-    reference = models.ForeignKey(
-        ReferenceInfo,
-        on_delete=models.CASCADE,
-        related_name="primer_reference"
-    )
 
