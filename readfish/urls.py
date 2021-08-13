98a629ae6a269af4d44330c4b133f57629a4c7d4
adoni5 <roryjmunro1@gmail.com>
2021-08-13 16:24:17 +0100

    add swordfish to urls

--- readfish/urls.py
+++ readfish/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls import re_path
 from readfish import views
 
 urlpatterns = [
-    re_path(r"^<str:run_id>/toml$", views.TomlDetail.as_view()),
-    re_path(r"^<run_id>/chopchop", views.GetToTheChopper.as_view()),
-    re_path(r"<run_id>/validate", views.ValidateSwordfishStartup.as_view())
+    re_path(r"^swordfish/<str:run_id>/toml$", views.TomlDetail.as_view()),
+    re_path(r"^swordfish/<run_id>/chopchop", views.GetToTheChopper.as_view()),
+    re_path(r"^swordfish/(<run_id>)/validate", views.ValidateSwordfishStartup.as_view())
 ]
