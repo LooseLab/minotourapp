9e6656daeeb98d410bf0d478cad8fe23fb318646
adoni5 <roryjmunro1@gmail.com>
2021-09-21 11:34:53 +0100

    Update urls to include all views and capture slugs with regex ,

--- readfish/urls.py
+++ readfish/urls.py
@@ -5,7 +5,8 @@ from django.conf.urls import re_path
 from readfish import views
 
 urlpatterns = [
-    re_path(r"^swordfish/<str:run_id>/toml$", views.TomlDetail.as_view()),
-    re_path(r"^swordfish/<run_id>/chopchop", views.GetToTheChopper.as_view()),
-    re_path(r"^swordfish/(<run_id>)/validate", views.ValidateSwordfishStartup.as_view())
+    re_path(r"^swordfish/(?P<run_id>\w+)/toml$", views.TomlDetail.as_view()),
+    re_path(r"^swordfish/(?P<run_id>\w+)/chopchop", views.GetToTheChopper.as_view()),
+    re_path(r"^swordfish/(?P<run_id>\w+)/validate/(?P<type>\w+)", views.ValidateSwordfishStartup.as_view()),
+    re_path(r"^swordfish/test-connect", views.ConnectHere.as_view())
 ]
